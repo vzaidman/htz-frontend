a38ad2a69addf12f2acb51bbe6cb2f350085b3e8
Brian Beck <exogen@users.noreply.github.com>
2017-08-28 00:53:41 -0700

    Fix typo

--- packages/htz-react-base/bin/htz-scripts.js
+++ packages/htz-react-base/bin/htz-scripts.js
@@ -9,7 +9,7 @@
 const spawn = require('cross-spawn')
 const argv = process.argv.slice(2)
 
-// Scipt names with namespaces/subtasks by convention are separated by ':', but
+// Script names with namespaces/subtasks by convention are separated by ':', but
 // we'd prefer to avoid them in filenames. Map namespaces to directories.
 const script = (argv[0] || '').replace(/:/g, '/')
 const args = argv.slice(1)
