6c1db84d92ee8d48a9155515d43de2506e46709a
Brian Beck <exogen@gmail.com>
2017-08-28 19:32:03 -0700

    Add fela-plugin-validator

--- packages/htz-components/src/components/StyleProvider/renderer.js
+++ packages/htz-components/src/components/StyleProvider/renderer.js
@@ -1,9 +1,13 @@
 import { createRenderer } from 'fela'
 import webPreset from 'fela-preset-web'
 
+const plugins = [...webPreset]
+
+if (process.env.NODE_ENV !== 'production') {
+  plugins.push(require('fela-plugin-validator').default())
+}
+
 /**
  * The Fela renderer used to create styles.
  */
-export default createRenderer({
-  plugins: [...webPreset]
-})
+export default createRenderer({ plugins })
