975665a0877c9cf3632655c9334a807c93f48a46
TxHawks <tofu.hawks@gmail.com>
2017-08-30 12:30:42 +0300

    Update format script

--- packages/htz-react-base/scripts/format.js
+++ packages/htz-react-base/scripts/format.js
@@ -10,7 +10,7 @@ function getFileList() {
     [
       '**/*.{js,jsx}',
       '!**/{.next,dist,node_modules}/**',
-      '!./{.next,dist,node_modules}/**'
+      '!./{.next,dist,node_modules}/**',
     ],
     { dot: true }
   );
@@ -20,8 +20,19 @@ if (!process.argv.length < 3) {
   // If no args are provided, use these defaults.
   // TODO: Update with Haaretz team's preferences.
   process.argv.push(
-    '--write', // Overwrite files.
-    '--single-quote' // Prefer single-quoted strings.
+    // Overwrite files.
+    '--write',
+    // The number of spaces per indentation level
+    '--tab-width',
+    '2',
+    // Prefer single-quoted strings.
+    '--single-quote',
+    // Prefer dangling commas everywhere except in functions
+    '--trailing-comma',
+    'es5',
+    // Put the > of a multi-line JSX element at the end
+    // of the last line instead of being alone on the next line.
+    '--jsx-bracket-same-line'
   );
   process.argv.push.apply(process.argv, getFileList());
 }
