e7e69e1c34cefa2e4400ea82a4ce2ec293b1ad66
TxHawks <tofu.hawks@gmail.com>
2017-08-30 12:01:12 +0300

    Add Haaretz eslint config settings

--- packages/htz-react-base/eslint.js
+++ packages/htz-react-base/eslint.js
@@ -1,3 +1,55 @@
 module.exports = {
-  extends: 'airbnb'
+  /* Extend airbnb's style-guide enforcement */
+  extends: 'airbnb',
+
+  rules: {
+    'brace-style': [2, 'stroustrup', { allowSingleLine: true, }, ],
+    /* Warn about long line */
+    'max-len': [1, 100, 2, ],
+    /* Warn about use of console */
+    'no-console': [1, ],
+    /* Warn about use of debugger */
+    'no-debugger': [1, ],
+    /* Allow nested ternaries */
+    'no-nested-ternary': 0,
+    /* Warn when declaring a variable with a name that already exists in the containing scope */
+    'no-shadow': [1, ],
+    /* Forbid referencing a variable before it is defined, but allow using declared functions */
+    'no-use-before-define': [2, 'nofunc', ],
+    /* Warn when referencing an undefined variable */
+    'no-undef': [1, ],
+    /* Forbid expressions that are never used */
+    'no-unused-expressions': [2, { allowShortCircuit: true, allowTernary: true, }, ],
+    /* Warn when declaring a variable without using it */
+    'no-unused-vars': [1, { vars: 'local', args: 'none', }, ],
+    'no-warning-comments': [1, { terms: ['fixme', 'todo', ], location: 'start', }, ],
+    'valid-jsdoc': [2, {
+      requireReturn: false,
+      requireParamDescription: false,
+      requireReturnDescription: false,
+    }, ],
+    /* Opinionated comma dangling rules */
+    'comma-dangle': ['error', {
+      arrays: 'always',
+      objects: 'always',
+      imports: 'always',
+      exports: 'always',
+      functions: 'ignore',
+    }, ],
+    /* disallow certain syntax forms */
+    'no-restricted-syntax': [
+      2,
+      'DebuggerStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    /* Import related rules */
+    'import/no-extraneous-dependencies': [
+      'error',
+      {'devDependencies': true, 'optionalDependencies': false, 'peerDependencies': false}
+    ],
+
+    /* React related rules */
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx',], }, ],
+  },
 };
