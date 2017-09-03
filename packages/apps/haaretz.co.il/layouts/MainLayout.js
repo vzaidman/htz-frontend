31eb8d6e8bc2ee8b4cb7c4cc60218da9301dd5b8
TxHawks <tofu.hawks@gmail.com>
2017-09-03 16:10:10 +0300

    style(haaretz.co.il):fix naming typo in MainLayout

--- packages/apps/haaretz.co.il/layouts/MainLayout.js
+++ packages/apps/haaretz.co.il/layouts/MainLayout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleProvider, } from '@haaretz/htz-components';
-import StyleRenderer from '../components/styleRenderer/styleRenderer';
+import styleRenderer from '../components/styleRenderer/styleRenderer';
 
 const propTypes = {
   children: PropTypes.node,
@@ -12,7 +12,7 @@ const defaultProps = {
 
 export default function MainLayout({ children, }) {
   return (
-    <StyleProvider renderer={StyleRenderer}>
+    <StyleProvider renderer={styleRenderer}>
       <div>
         {children}
       </div>
