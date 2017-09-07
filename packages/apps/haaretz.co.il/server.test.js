975882ec68307f0ed99bf1aa615aefda04f21a2e
Brian Beck <exogen@gmail.com>
2017-09-06 20:55:18 -0700

    add more routePattern tests

--- packages/apps/haaretz.co.il/server.test.js
+++ packages/apps/haaretz.co.il/server.test.js
@@ -70,4 +70,34 @@ describe('routePattern', () => {
       '12.345',
     ]);
   });
+
+  test('works with a section with a contentId-like name', () => {
+    const url = '/12.345/67.890';
+    expect(routePattern.exec(url).slice()).toEqual([
+      url,
+      '12.345',
+      undefined,
+      '67.890',
+    ]);
+  });
+
+  test('works with premium content in a section with a contentId-like name', () => {
+    const url = '/12.345/.premium-67.890';
+    expect(routePattern.exec(url).slice()).toEqual([
+      url,
+      '12.345',
+      '.premium-',
+      '67.890',
+    ]);
+  });
+
+  test('works with multiple sections with contentId-like names', () => {
+    const url = '/1.23/4.56/7.89';
+    expect(routePattern.exec(url).slice()).toEqual([
+      url,
+      '1.23/4.56',
+      undefined,
+      '7.89',
+    ]);
+  });
 });
