09a10ccbc5b450fb435fd90061eb82f69b04ea47
TxHawks <tofu.hawks@gmail.com>
2017-10-29 14:51:54 +0200

    fix(htz-theme): freeze the fontStacks object to prevent it from being accidentaly modified
    affects: @haaretz/htz-theme

--- packages/components/htz-theme/src/consts/fontStacks.js
+++ packages/components/htz-theme/src/consts/fontStacks.js
@@ -1,6 +1,6 @@
-const fontStacks = {
+const fontStacks = Object.freeze({
   default: '"Open Sans Hebrew","Helvetica Neue",Helvetica,Arial,sans-serif',
   alt: '"Helvetica Neue",Helvetica,Arial,sans-serif',
-};
+});
 
 export default fontStacks;
