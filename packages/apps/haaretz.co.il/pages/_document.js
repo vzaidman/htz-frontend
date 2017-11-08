e4a695779b9f834ba8f468e4cd3045038fe1904e
kedarguy <kedarguy@gmail.com>
2017-11-08 16:33:07 +0200

    fix(css): fix loading of static CSS rules by Next JS
    affects: @haaretz/haaretz.co.il, @haaretz/htz-components
    
    Fix an issue with static rules not being reloaded on subsequent renders in Next JS pages

--- packages/apps/haaretz.co.il/pages/_document.js
+++ packages/apps/haaretz.co.il/pages/_document.js
@@ -8,21 +8,36 @@ import styleRenderer from '../components/styleRenderer/styleRenderer';
 // maybe also using the eBay method
 // (http://www.ebaytechblog.com/2017/09/21/ebays-font-loading-strategy/)
 // to only load a polyfill when the `CSS Font Loading API` is not supported.
-styleRenderer.renderFont(
-  '"Open Sans Hebrew"',
+const fonts = [
   [
-    './static/fonts/OpenSansHebrewRegular.woff',
-    './static/fonts/OpenSansHebrewRegular.woff2',
+    '"Open Sans Hebrew"',
+    [
+      './static/fonts/OpenSansHebrewRegular.woff',
+      './static/fonts/OpenSansHebrewRegular.woff2',
+    ],
+    { fontWeight: 400, },
   ],
-  { fontWeight: 400, }
-);
-styleRenderer.renderFont(
-  '"Open Sans Hebrew"',
-  [ './static/fonts/OpenSansHebrewBold.woff', './static/fonts/OpenSansHebrewBold.woff2', ],
-  { fontWeight: 700, }
-);
+  [
+    '"Open Sans Hebrew"',
+    [ './static/fonts/OpenSansHebrewBold.woff', './static/fonts/OpenSansHebrewBold.woff2', ],
+    { fontWeight: 700, },
+  ],
+];
+// styleRenderer.renderFont(
+//   '"Open Sans Hebrew"',
+//   [
+//     './static/fonts/OpenSansHebrewRegular.woff',
+//     './static/fonts/OpenSansHebrewRegular.woff2',
+//   ],
+//   { fontWeight: 400, }
+// );
+// styleRenderer.renderFont(
+//   '"Open Sans Hebrew"',
+//   [ './static/fonts/OpenSansHebrewBold.woff', './static/fonts/OpenSansHebrewBold.woff2', ],
+//   { fontWeight: 700, }
+// );
 
-styleRenderer.renderStatic(cssReset);
+// styleRenderer.renderStatic(cssReset);
 
-const HaaretzDocument = createDocument(styleRenderer);
+const HaaretzDocument = createDocument(styleRenderer, fonts, cssReset);
 export default HaaretzDocument;
