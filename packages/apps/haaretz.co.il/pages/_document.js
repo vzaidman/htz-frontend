41542ee99004470248218a83e27a9f51c3761f38
kedarguy <kedarguy@gmail.com>
2017-11-12 14:52:34 +0200

    refactor(haaretz.co.il/pages/_document.js): remove commented out old code
    affects: @haaretz/haaretz.co.il

--- packages/apps/haaretz.co.il/pages/_document.js
+++ packages/apps/haaretz.co.il/pages/_document.js
@@ -23,21 +23,6 @@ const fonts = [
     { fontWeight: 700, },
   ],
 ];
-// styleRenderer.renderFont(
-//   '"Open Sans Hebrew"',
-//   [
-//     './static/fonts/OpenSansHebrewRegular.woff',
-//     './static/fonts/OpenSansHebrewRegular.woff2',
-//   ],
-//   { fontWeight: 400, }
-// );
-// styleRenderer.renderFont(
-//   '"Open Sans Hebrew"',
-//   [ './static/fonts/OpenSansHebrewBold.woff', './static/fonts/OpenSansHebrewBold.woff2', ],
-//   { fontWeight: 700, }
-// );
-
-// styleRenderer.renderStatic(cssReset);
 
 const HaaretzDocument = createDocument(styleRenderer, fonts, cssReset);
 export default HaaretzDocument;
