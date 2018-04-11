3c7c9c463b06a879f7b577a25fb6a98d1882a92d
eliagrady <elia.grady@haaretz.co.il>
2018-04-11 18:52:56 +0300

    fix: fix user injector and dispenser
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/User/UserDispenser.js
+++ packages/components/htz-components/src/components/User/UserDispenser.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment, } from 'react';
 import { UserFactory, UserTypes, } from '@haaretz/htz-user-utils';
 import PropTypes from 'prop-types';
-import { graphql, compose, } from 'react-apollo';
+import { Mutation, Query, } from 'react-apollo';
 import { UPDATE_USER, GET_USER, } from './UserInjector';
 import ImageCookies from './ImageCookies';
 
@@ -19,26 +19,26 @@ export const userScheme = {
 
 const propTypes = {
   render: PropTypes.func.isRequired,
-  /** Indicates data loading state */
-  loading: PropTypes.bool,
-  /** Indicates data error state */
-  error: PropTypes.bool,
-  user: PropTypes.shape({
-    __typename: PropTypes.string,
-    userName: PropTypes.string,
-    id: PropTypes.string,
-    lastName: PropTypes.string,
-    firstName: PropTypes.string,
-    emailStatus: PropTypes.string,
-    premiumArticlesCount: PropTypes.number,
-    type: PropTypes.string,
-    anonymousId: PropTypes.string,
-  }).isRequired,
-  mutate: PropTypes.func.isRequired,
+  // /** Indicates data loading state */
+  // loading: PropTypes.bool,
+  // /** Indicates data error state */
+  // error: PropTypes.bool,
+  // user: PropTypes.shape({
+  //   __typename: PropTypes.string,
+  //   userName: PropTypes.string,
+  //   id: PropTypes.string,
+  //   lastName: PropTypes.string,
+  //   firstName: PropTypes.string,
+  //   emailStatus: PropTypes.string,
+  //   premiumArticlesCount: PropTypes.number,
+  //   type: PropTypes.string,
+  //   anonymousId: PropTypes.string,
+  // }).isRequired,
+  // mutate: PropTypes.func.isRequired,
 };
 const defaultProps = {
-  loading: false,
-  error: false,
+  // loading: false,
+  // error: false,
 };
 
 class UserDispenser extends Component {
@@ -56,22 +56,22 @@ class UserDispenser extends Component {
   }
 
   handleImgOnload = () => {
-    const user = new UserFactory(true).build();
-    console.log('handleImgOnload user:', user);
-    this.props.mutate({
-      variables: { user, },
-      // TODO check if there's a way to pass a real object reference for overriding (no-skeleton)
-      update: (proxy, { data: { updateUser, }, }) => {
-        // Read the data from our cache for this query.
-        const data = proxy.readQuery({ query: GET_USER, });
+    // console.log('handleImgOnload user:', user);
+    console.log('hsdgsdgsdgsdg');
+    // this.props.mutate({
+    //   variables: { user, },
+    //   // TODO check if there's a way to pass a real object reference for overriding (no-skeleton)
+    //   update: (proxy, { data: { updateUser, }, }) => {
+    //     // Read the data from our cache for this query.
+    //     const data = proxy.readQuery({ query: GET_USER, });
 
-        // Add our data from the mutation to the data object.
-        data.user = { ...userScheme, ...user, };
+    //     // Add our data from the mutation to the data object.
+    //     data.user = { ...userScheme, ...user, };
 
-        // Write our data back to the cache.
-        proxy.writeQuery({ query: GET_USER, data, });
-      },
-    });
+    //     // Write our data back to the cache.
+    //     proxy.writeQuery({ query: GET_USER, data, });
+    //   },
+    // });
     return Promise.resolve(console.log('onload handleImgOnload'));
   };
 
@@ -83,28 +83,45 @@ class UserDispenser extends Component {
     });
 
   render() {
-    if (this.state.shouldRender && !this.props.loading) {
-      if (this.props.error) {
-        console.error(this.props.error);
-        return null;
-      }
-      const isLoggedIn =
-        this.props.user &&
-        (this.props.user.type === UserTypes.paying ||
-          this.props.user.type === UserTypes.registered);
-      const { user, } = this.props;
+    // if (this.state.shouldRender && !this.props.loading) {
+    if (this.state.shouldRender) {
+      // if (this.props.error) {
+      //   console.error(this.props.error);
+      //   return null;
+      // }
+      // const isLoggedIn =
+      //   this.props.user &&
+      //   (this.props.user.type === UserTypes.paying ||
+      //     this.props.user.type === UserTypes.registered);
       return (
         <Fragment>
-          <ImageCookies
-            images={this.state.images}
-            onload={this.handleImgOnload}
-          />
-          {this.props.render({
-            isLoggedIn,
-            user,
-            plantImages: this.plantImages,
-            handleImgOnload: this.handleImgOnload,
-          })}
+          <Mutation mutation={UPDATE_USER}>
+            {updateUser => (
+              <ImageCookies
+                images={this.state.images}
+                onload={() => {
+                  const newUser = new UserFactory(true).build();
+                  updateUser({
+                    variables: { user: { ...userScheme, ...newUser, }, },
+                  });
+                  this.state.resolveOnImageLoad();
+                }}
+              />
+            )}
+          </Mutation>
+          <Query query={GET_USER} ssr={false}>
+            {({ data: { user, }, }) =>
+              this.props.render({
+                isLoggedIn:
+                  user &&
+                  (user.type === UserTypes.paying ||
+                    user.type === UserTypes.registered),
+                user,
+                plantImages: this.plantImages,
+                // handleImgOnload: this.handleImgOnload,
+              })
+            }
+          </Query>
         </Fragment>
       );
     }
@@ -115,10 +132,10 @@ class UserDispenser extends Component {
 UserDispenser.propTypes = propTypes;
 UserDispenser.defaultProps = defaultProps;
 
-const WrappedUserDispenser = compose(
-  graphql(UPDATE_USER),
-  graphql(GET_USER, {
-    props: ({ data, }) => data,
-  })
-)(UserDispenser);
-export default WrappedUserDispenser;
+// const WrappedUserDispenser = compose(
+//   graphql(GET_USER, {
+//     props: ({ data, }) => data,
+//   })
+// )(UserDispenser);
+// export default WrappedUserDispenser;
+export default UserDispenser;
--- packages/components/htz-components/src/components/User/UserInjector.js
+++ packages/components/htz-components/src/components/User/UserInjector.js
@@ -1,7 +1,7 @@
 /* global window */
-import { Component, } from 'react';
+import React, { Component, } from 'react';
 import PropTypes from 'prop-types';
-import { graphql, compose, } from 'react-apollo';
+import { Mutation, } from 'react-apollo';
 import gql from 'graphql-tag';
 import { UserFactory, } from '@haaretz/htz-user-utils';
 
@@ -30,80 +30,120 @@ export const GET_USER = gql`
   }
 `;
 
+// const propTypes = {
+//   /** Indicates data loading state */
+//   loading: PropTypes.bool,
+//   /** Indicates data error state */
+//   error: PropTypes.bool,
+//   /** A user object that the injector holds */
+//   user: PropTypes.shape({
+//     type: PropTypes.string,
+//     id: PropTypes.oneOfType([ PropTypes.string, PropTypes.number, ]),
+//     email: PropTypes.email,
+//     firstName: PropTypes.string,
+//     lastName: PropTypes.string,
+//     emailStatus: PropTypes.bool,
+//     token: PropTypes.string,
+//     anonymousId: PropTypes.oneOfType([ PropTypes.string, PropTypes.number, ]),
+//     __typename: PropTypes.string.isRequired,
+//   }),
+//   /** A mutate function for updating the currently set user */
+//   mutate: PropTypes.func.isRequired,
+// };
 const propTypes = {
-  /** Indicates data loading state */
-  loading: PropTypes.bool,
-  /** Indicates data error state */
-  error: PropTypes.bool,
-  /** A user object that the injector holds */
-  user: PropTypes.shape({
-    type: PropTypes.string,
-    id: PropTypes.oneOfType([ PropTypes.string, PropTypes.number, ]),
-    email: PropTypes.email,
-    firstName: PropTypes.string,
-    lastName: PropTypes.string,
-    emailStatus: PropTypes.bool,
-    token: PropTypes.string,
-    anonymousId: PropTypes.oneOfType([ PropTypes.string, PropTypes.number, ]),
-    __typename: PropTypes.string.isRequired,
-  }),
   /** A mutate function for updating the currently set user */
-  mutate: PropTypes.func.isRequired,
+  updateUser: PropTypes.func.isRequired,
 };
-const defaultProps = {
-  loading: false,
-  error: false,
-  user: {
-    email: null,
-    emailStatus: null,
-    firstName: null,
-    lastName: null,
-    id: null,
-    anonymousId: null,
-    type: null,
-    token: null,
-  },
-};
+// const defaultProps = {
+//   loading: false,
+//   error: false,
+
+// };
 
 class UserInjector extends Component {
-  state = { shouldRender: false, };
+  state = {
+    shouldRender: false,
+    // user: {
+    //   email: null,
+    //   emailStatus: null,
+    //   firstName: null,
+    //   lastName: null,
+    //   id: null,
+    //   anonymousId: null,
+    //   type: null,
+    //   token: null,
+    // },
+  };
 
   componentDidMount() {
     if (!this.state.shouldRender) {
       // Build a user from a newly parsed (hence :'true' param) cookie state
       const user = new UserFactory(true).build();
+      this.props.updateUser({ variables: { user, }, });
       // Change apollo's user data in memory (store)
-      this.props.mutate({ variables: { user, }, });
+      // this.props.mutate({
+      //   name: 'updateUser',
+      //   variables: { user, },
+      //   options: {
+      //     refetchQueries: [ { query: GET_USER, }, ],
+      //   },
+      // });
       // eslint-disable-next-line react/no-did-mount-set-state
       this.setState({ shouldRender: true, });
     }
   }
 
   render() {
-    if (this.state.shouldRender) {
-      const { loading, error, user, } = this.props;
-      if (loading) return null;
-      if (error) {
-        console.error(error);
-        return null;
-      }
-      if (user) {
-        return null;
-      }
-      console.error('User Injector failed creating a user:', this.props.user);
-      return null;
-    }
     return null;
   }
+  // render() {
+  //   if (this.state.shouldRender) {
+  //     const { loading, error, user, } = this.props;
+  //     if (loading) return null;
+  //     if (error) {
+  //       console.error(error);
+  //       return null;
+  //     }
+  //     if (user) {
+  //       return null;
+  //     }
+  //     console.error('User Injector failed creating a user:', this.props.user);
+  //     return null;
+  //   }
+  //   return null;
+  // }
+}
+UserInjector.propTypes = propTypes;
+
+function Wrapper() {
+  return (
+    <Mutation
+      mutation={UPDATE_USER}
+      // update={(cache, { data: { updateUser, }, }) => {
+      //   const { user, } = cache.readQuery({ query: GET_USER, });
+      //   cache.writeQuery({
+      //     query: GET_USER,
+      //     data: { user, },
+      //   });
+      // }}
+    >
+      {updateUser => <UserInjector updateUser={updateUser} />}
+    </Mutation>
+  );
 }
 
-UserInjector.propTypes = propTypes;
-UserInjector.defaultProps = defaultProps;
-const WrappedUserInjector = compose(
-  graphql(UPDATE_USER),
-  graphql(GET_USER, {
-    props: ({ data, }) => data,
-  })
-)(UserInjector);
+// UserInjector.defaultProps = defaultProps;
+// const WrappedUserInjector = compose(
+// const WrappedUserInjector = compose(
+//   graphql(UPDATE_USER),
+//   graphql(GET_USER, {
+//     props: ({ data, }) => {
+//       // eslint-disable-next-line
+//       debugger;
+//       return data;
+//     },
+//   })
+// )(UserInjector);
 
-export default WrappedUserInjector;
+// export default WrappedUserInjector;
+export default Wrapper;
