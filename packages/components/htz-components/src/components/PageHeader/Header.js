04ab9f29b163063d49a83051a4e04edc8406e4f7
EranM6 <eranm6@gmail.com>
2018-04-16 02:26:31 +0300

    feat(navigation-search): searchbar for navigation header
    affects: @haaretz/htz-components, @haaretz/htz-theme

--- packages/components/htz-components/src/components/PageHeader/Header.js
+++ packages/components/htz-components/src/components/PageHeader/Header.js
@@ -13,10 +13,7 @@ const wrapperStyle = ({ theme, }) => ({
   backgroundColor: theme.color('neutral', '-10'),
   ...borderBottom('1px', 0, 'solid', theme.color('primary')),
   paddingTop: `${10 / 7}rem`,
-<<<<<<< fcf6262a968023f27cea431743369601b26c9938
-=======
   paddingBottom: `${10 / 7}rem`,
->>>>>>> feat(page header): search
   display: 'flex',
 });
 const Wrapper = createComponent(wrapperStyle);
