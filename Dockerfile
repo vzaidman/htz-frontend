c8f91fba0de4ee19395979beb05811afa84320ec
mozzi <maozza@gmail.com>
2018-04-21 21:00:32 +0300

    chore(build pipeline): add pipeline

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:8.9
 COPY  . /workspace/
 WORKDIR /workspace/
 RUN yarn && yarn bootstrap
-#RUN yarn test:preDeploy
+#RUN yarn test:deploy
 EXPOSE 3000
 WORKDIR /workspace/
 CMD ["yarn", "workspace", "@haaretz/haaretz.co.il", "start"]
---
+++ Jenkinsfile.groovy
@@ -0,0 +1,70 @@
+import groovy.json.JsonSlurper
+
+node {
+    properties(
+            [
+//                    parameters([
+//                            string(name: 'MAVEN_ADDITIONAL_PARAM', defaultValue: '', description: 'additional parameters to maven command line, i.e. "-X" to add debug information to the log')
+//                    ]),
+pipelineTriggers([
+        triggers: [
+                [
+                        $class      : 'hudson.triggers.SCMTrigger',
+                        scmpoll_spec: 'H/3 * * * *'
+                ]
+        ]
+]),
+[$class  : 'BuildDiscarderProperty',
+ strategy:
+         [$class               : 'LogRotator',
+          artifactDaysToKeepStr: '',
+          artifactNumToKeepStr : '',
+          daysToKeepStr        : '',
+          numToKeepStr         : '10']
+],
+disableConcurrentBuilds()
+            ])
+    catchError {
+        withEnv(["PATH+NODE=${tool 'node-8.9'}/bin"]) {
+            stage("Initialize") {
+                checkout scm
+                def dockerRegistry = "docker.themarker.com:8083/"
+                stage('Prepare') {
+                    //sh "npm install -g yarn"
+                    //sh "yarn install"
+                    //def json = readFile(file:'package.json')
+                    //def data = new JsonSlurperClassic().parseText(json)
+                    env.test_command = "yarn test:deploy"
+                }
+                stage('Build') {
+                    sh '''
+                    #!/usr/bin/env bash
+                    set -e                                      
+                    #export DOCKER_HOST=unix:///var/run/docker.sock                    
+                    docker-compose build                    
+                    '''
+                }
+                stage('Test') {
+                    sh 'docker-compose run ${test_command}'
+                }
+                stage('Deploy') {
+                    sh '''#!/usr/bin/env bash
+                    set -e                    
+                    docker-compose push
+                    export version=${container_version}
+                    docker-compose push
+                    '''
+                }
+            }
+        }
+    }
+    stage('Post-Build') {
+        sh 'docker-compose down -v'
+        //step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: 'maoz@leadspace.com', sendToIndividuals: true])
+        step([$class                  : 'Mailer',
+              notifyEveryUnstableBuild: true,
+              recipients              : emailextrecipients([[$class: 'CulpritsRecipientProvider'],
+                                                            [$class: 'RequesterRecipientProvider']])])
+    }
+
+}
