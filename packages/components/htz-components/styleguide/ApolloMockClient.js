5782a63f7bfec045a7b7a008e42c150fd2fa873e
tamirbenCA <tamirben@gmail.com>
2018-06-04 11:02:19 +0300

    feat(htz-components): make mock apollo client more reusable
    affects: @haaretz/htz-components
    
    allows using mock apollo client in tests

---
+++ packages/components/htz-components/styleguide/ApolloMockClient.js
@@ -0,0 +1,80 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { schema, } from '@haaretz/app-utils';
+import { ApolloLink, } from 'apollo-link';
+import { ApolloClient, } from 'apollo-client';
+import { InMemoryCache, } from 'apollo-cache-inmemory';
+import { withClientState, } from 'apollo-link-state';
+import { SchemaLink, } from 'apollo-link-schema';
+import { addMockFunctionsToSchema, } from 'graphql-tools';
+import mocks from './mocks';
+import { userScheme, } from '../src/components/User/UserDispenser';
+
+addMockFunctionsToSchema({
+  schema,
+  mocks,
+});
+const link = new SchemaLink({ schema, });
+
+const cache = new InMemoryCache();
+
+const stateLink = withClientState({
+  cache,
+  defaults: {
+    scroll: {
+      velocity: null,
+      x: 0,
+      y: 0,
+      __typename: 'Scroll',
+    },
+    user: {
+      type: 'paying',
+      id: '1',
+      email: 'email@haaretz.co.il',
+      firstName: 'FIRSTNAME',
+      lastName: 'LASTNAME',
+      emailStatus: null,
+      token: 'imatoken',
+      anonymousId: '22',
+      __typename: 'User',
+    },
+    a11yToggle: false,
+  },
+  resolvers: {
+    Mutation: {
+      // eslint-disable-next-line no-shadow
+      updateScroll: (_, { x, y, direction, velocity, }, { cache, }) => {
+        const data = {
+          scroll: {
+            velocity,
+            x,
+            y,
+            __typename: 'Scroll',
+          },
+        };
+        cache.writeData({ data, });
+        // resolver needs to return something / null https://github.com/apollographql/apollo-link-state/issues/160
+        return null;
+      },
+      updateUser: (_, { user, }, { cache, }) => {
+        console.log('update User:', user);
+        const data = {
+          user: {
+            ...userScheme,
+            ...user,
+            id: '1',
+          },
+        };
+        cache.writeData({ data, });
+        // resolver needs to return something / null https://github.com/apollographql/apollo-link-state/issues/160
+        return null;
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([ stateLink, link, ]),
+  cache,
+});
+
+export default client;
