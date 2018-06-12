e86ab67624542731cd8e30a261fa83406f1116f3
EranM6 <eranm6@gmail.com>
2018-06-12 11:28:19 +0300

    refactor(breadcrumbs): add query to breadcrumbs
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/Breadcrumbs/Breadcrumbs.js
+++ packages/components/htz-components/src/components/Breadcrumbs/Breadcrumbs.js
@@ -1,71 +1,99 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FelaComponent, } from 'react-fela';
+import { FelaComponent, FelaTheme, } from 'react-fela';
+import gql from 'graphql-tag';
+import { Query, } from 'react-apollo';
+
 import Link from '../Link/Link';
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
 const propTypes = {
-  /**
-   * An array of objects with the sections name and path.
-   */
-  steps: PropTypes.arrayOf(
-    PropTypes.shape({
-      pathSegment: PropTypes.string,
-      contentId: PropTypes.string,
-      name: PropTypes.string,
-      url: PropTypes.string,
-    })
-  ).isRequired,
+  articleId: PropTypes.string.isRequired,
 };
 
-function Breadcrumbs({ steps, }) {
-  // creating a copy because when the 'steps' array is received from apollo, he is sealed.
-  const crumbs = [ ...steps, ].reverse();
-  crumbs.shift();
-  crumbs.pop();
+const GET_BREADCRUMBS = gql`
+  query Breadcrumbs($path: String!) {
+    page(path: $path) {
+      lineage {
+        pathSegment
+        contentId
+        name
+        url
+      }
+    }
+  }
+`;
+
+function Breadcrumbs({ articleId, }) {
   return (
-    <FelaComponent
-      render={({ theme: { breadcrumbsI18n: { ariaLabel, }, }, }) => (
-        <nav aria-label={ariaLabel}>
-          {crumbs.map((crumb, index) => (
-            <FelaComponent
-              key={crumb.contentId}
-              style={theme => ({
-                ...theme.type(-1),
-                fontWeight: '700',
-                marginInlineEnd: '1rem',
-                color: theme.color('neutral', '-4'),
-                ':hover': {
-                  color: theme.color('neutral', '-3'),
-                  textDecoration: 'underline',
-                  underlineSkip: 'ink',
-                },
-                extend: [
-                  theme.mq(
-                    { until: 's', },
-                    index !== crumbs.length - 1 ? { display: 'none', } : {}
-                  ),
-                  {
-                    ':nth-child(odd)': {
-                      color: theme.color('neutral', '-2'),
-                      ':hover': {
-                        color: theme.color('neutral', '-1'),
-                      },
-                    },
-                  },
-                ],
-              })}
-              render={({ className, }) => (
-                <Link
-                  className={className}
-                  content={crumb.name}
-                  href={crumb.url}
-                />
-              )}
+    <ErrorBoundary FallbackComponent={() => <p>Error</p>}>
+      <Query
+        query={GET_BREADCRUMBS}
+        variables={{ path: articleId, }}
+        errorPolicy="all"
+      >
+        {({ data, loading, error, }) => {
+          if (loading) return <p>loading...</p>;
+          if (error) return <p>Error</p>;
+          if (!data || !data.page) throw new TypeError('no data !!');
+          const { lineage, } = data.page;
+          // creating a copy because when the 'steps' array is received from apollo, he is sealed.
+          const crumbs = [ ...lineage, ].reverse();
+          crumbs.shift();
+          crumbs.pop();
+          return (
+            <FelaTheme
+              render={theme => {
+                const { breadcrumbsI18n: { ariaLabel, }, } = theme;
+                return (
+                  <nav aria-label={ariaLabel}>
+                    {crumbs.map((crumb, index) => (
+                      <FelaComponent
+                        key={crumb.contentId}
+                        style={{
+                          ...theme.type(-1),
+                          fontWeight: '700',
+                          marginInlineEnd: '1rem',
+                          color: theme.color('neutral', '-4'),
+                          ':hover': {
+                            color: theme.color('neutral', '-3'),
+                            textDecoration: 'underline',
+                            underlineSkip: 'ink',
+                          },
+                          extend: [
+                            theme.mq(
+                              { until: 's', },
+                              index !== crumbs.length - 1
+                                ? { display: 'none', }
+                                : {}
+                            ),
+                            {
+                              ':nth-child(odd)': {
+                                color: theme.color('neutral', '-2'),
+                                ':hover': {
+                                  color: theme.color('neutral', '-1'),
+                                },
+                              },
+                            },
+                          ],
+                        }}
+                        render={({ className, }) => (
+                          <Link
+                            className={className}
+                            content={crumb.name}
+                            href={crumb.url}
+                          />
+                        )}
+                      />
+                    ))}
+                  </nav>
+                );
+              }}
             />
-          ))}
-        </nav>
-      )}
-    />
+          );
+        }}
+      </Query>
+    </ErrorBoundary>
   );
 }
 
