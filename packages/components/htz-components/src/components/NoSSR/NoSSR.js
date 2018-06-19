b65ac5348e1f41a82034061eade29855603f0c01
TawKhoury <toufe2k@gmail.com>
2018-06-19 18:27:13 +0300

    feat(no-ssr): adding no ssr component
    affects: @haaretz/htz-components

---
+++ packages/components/htz-components/src/components/NoSSR/NoSSR.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NoSSR extends React.Component {
+  static propTypes = {
+    /**
+     * Nodes rendered inside `NoSSR`.
+     * Passed to the underlying react element
+     */
+    children: PropTypes.node,
+    /**
+     * Render node on server side rendering.
+     */
+    onSSR: PropTypes.node,
+  };
+  static defaultProps = {
+    children: null,
+    onSSR: null,
+  };
+  state = {
+    shouldRender: false,
+  };
+
+  componentDidMount() {
+    // eslint-disable-next-line react/no-did-mount-set-state
+    this.setState({ shouldRender: true, });
+  }
+
+  render() {
+    const { children, onSSR, } = this.props;
+    const { shouldRender, } = this.state;
+
+    return shouldRender ? children : onSSR;
+  }
+}
+
+export default NoSSR;
