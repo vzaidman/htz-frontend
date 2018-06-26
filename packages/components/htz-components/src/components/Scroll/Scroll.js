b29b9ac5f2569ef58889f090980c6a92cd2cc9a2
EranM6 <eranm6@gmail.com>
2018-06-26 12:33:47 +0300

    refactor(scroll): change old graphql function with Query component
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/Scroll/Scroll.js
+++ packages/components/htz-components/src/components/Scroll/Scroll.js
@@ -1,7 +1,6 @@
-/* global window */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { graphql, } from 'react-apollo';
+import { Query, } from 'react-apollo';
 import gql from 'graphql-tag';
 
 export const SCROLL_QUERY = gql`
@@ -16,27 +15,6 @@ export const SCROLL_QUERY = gql`
 
 const propTypes = {
   /**
-   * Indicates data loading state
-   * Passed implicitly by Apollo, not directly as an attribute on the component
-   */
-  loading: PropTypes.bool.isRequired,
-  /**
-   * Indicates data error state
-   * Passed implicitly by Apollo, not directly as an attribute on the component
-   */
-  // eslint-disable-next-line react/forbid-prop-types
-  error: PropTypes.object,
-
-  /**
-   * Holds the scroll Object with x, y coords and scroll velocity
-   * Passed implicitly by Apollo, not directly as an attribute on the component
-   */
-  scroll: PropTypes.shape({
-    x: PropTypes.number,
-    y: PropTypes.number,
-    velocity: PropTypes.number,
-  }),
-  /**
    * The render Props callback
    * This component was built using the render props pattern
    *
@@ -47,25 +25,20 @@ const propTypes = {
    */
   render: PropTypes.func.isRequired,
 };
-const defaultProps = {
-  error: null,
-  scroll: null,
-};
 
-export function Scroll({ loading, error, scroll, render, }) {
-  if (loading) return <div>Loading ...</div>;
-  if (error) return <div>Error</div>;
-  if (scroll) {
-    const { x, y, velocity, } = scroll;
-    return render({ x, y, velocity, });
-  }
+export function Scroll({ render, }) {
+  return (
+    <Query query={SCROLL_QUERY}>
+      {({ loading, error, data: { scroll, }, }) => {
+        if (loading) return null;
+        if (error) return null;
+        const { x, y, velocity, } = scroll;
+        return render({ x, y, velocity, });
+      }}
+    </Query>
+  );
 }
 
 Scroll.propTypes = propTypes;
-Scroll.defaultProps = defaultProps;
 
-const WrappedScroll = graphql(SCROLL_QUERY, {
-  props: ({ data, }) => data,
-})(Scroll);
-
-export default WrappedScroll;
+export default Scroll;
