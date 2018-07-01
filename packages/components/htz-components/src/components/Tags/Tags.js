227a1224c4e61db538f33ea541b152c78f351b4b
EranM6 <eranm6@gmail.com>
2018-07-01 17:07:08 +0300

    fix(tags): doesn't show anything if there's no tags
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/Tags/Tags.js
+++ packages/components/htz-components/src/components/Tags/Tags.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { createComponent, withTheme, } from 'react-fela';
+import { FelaComponent, } from 'react-fela';
 import HtzLink from '../HtzLink/HtzLink';
 import H from '../AutoLevels/H';
 
@@ -20,57 +20,62 @@ const propTypes = {
       contentName: PropTypes.string.isRequired,
     })
   ).isRequired,
-  /** passed as a a prop by fela's withTheme func before default export */
-  theme: PropTypes.shape({
-    tagsElementI18n: PropTypes.object.isRequired,
-  }).isRequired,
 };
-
-const titleStyle = ({ theme, }) => ({
-  color: theme.color('secondary'),
-  marginEnd: '1rem',
-});
-const TagsTitle = createComponent(titleStyle, H);
-
-const contentStyle = ({ theme, }) => ({
-  color: theme.color('neutral', '-2'),
-  textDecoration: 'underline',
-});
-const TagContent = createComponent(contentStyle, 'span');
-
-const tagStyle = ({ theme, isLast, }) => ({
-  display: 'inline',
-  ...(!isLast && { marginEnd: '1rem', }),
-});
-const Tag = createComponent(tagStyle, 'li');
-
-const tagsWrapperStyle = ({ theme, }) => ({
-  ...theme.type(-1),
-  display: 'flex',
-});
-const TagsWrapper = createComponent(tagsWrapperStyle);
 
 /*
  * Returns an inline list of the article's related tags.
  */
-function Tags({ tagsList, theme, }) {
-  return (
-    <TagsWrapper>
-      <TagsTitle>{theme.tagsElementI18n.prefix}</TagsTitle>
-      <ul>
-        {tagsList.map((tag, i) => (
-          <Tag key={tag.contentName} isLast={tagsList.length === i + 1}>
-            <HtzLink
-              href={tag.url}
-              content={<TagContent>{tag.contentName}</TagContent>}
-            />
-          </Tag>
-        ))}
-      </ul>
-    </TagsWrapper>
-  );
+function Tags({ tagsList, }) {
+  return tagsList && tagsList.length > 0 ? (
+    <FelaComponent
+      style={theme => ({
+        ...theme.type(-1),
+        display: 'flex',
+      })}
+      render={({ className, theme, }) => (
+        <div className={className}>
+          <FelaComponent
+            style={{
+              color: theme.color('secondary'),
+              marginEnd: '1rem',
+            }}
+            render={({ className, }) => (
+              <H className={className}>{theme.tagsElementI18n.prefix}</H>
+            )}
+          />
+          <ul>
+            {tagsList.map((tag, i) => (
+              <FelaComponent
+                style={{
+                  display: 'inline',
+                  ...(tagsList.length !== i + 1 ? { marginEnd: '1rem', } : {}),
+                }}
+                key={tag.contentName}
+                render="li"
+              >
+                <HtzLink
+                  href={tag.url}
+                  content={
+                    <FelaComponent
+                      style={{
+                        color: theme.color('neutral', '-2'),
+                        textDecoration: 'underline',
+                      }}
+                      render="span"
+                    >
+                      {tag.contentName}
+                    </FelaComponent>
+                  }
+                />
+              </FelaComponent>
+            ))}
+          </ul>
+        </div>
+      )}
+    />
+  ) : null;
 }
 
 Tags.propTypes = propTypes;
 
-export default withTheme(Tags);
+export default Tags;
