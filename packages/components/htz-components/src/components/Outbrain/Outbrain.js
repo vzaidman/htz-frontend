dabc179fca620df32a24eca5f5d6ede08bc57ef3
EranM6 <eranm6@gmail.com>
2018-07-01 11:49:52 +0300

    fix(outbrain): change widget id to AR_4
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/Outbrain/Outbrain.js
+++ packages/components/htz-components/src/components/Outbrain/Outbrain.js
@@ -28,7 +28,7 @@ class Outbrain extends React.Component {
           <div
             className="OUTBRAIN"
             data-src={canonicalUrl}
-            data-widget-id="AR_10"
+            data-widget-id="AR_4"
             data-ob-template="haaretz-heb"
           />
         )}
