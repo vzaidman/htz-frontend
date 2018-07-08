05dcbc30d297273fdd951e0d6c8610dfbd3cd50d
kedarguy <kedarguy@gmail.com>
2018-07-08 12:59:56 +0300

    fix(app-utils): switchToDomain always returned haaretz.co.il
    affects: @haaretz/app-utils

--- packages/libs/app-utils/src/utils/switchToDomain.js
+++ packages/libs/app-utils/src/utils/switchToDomain.js
@@ -1,9 +1,7 @@
 import breakUrl from './breakUrl';
 
 export default function switchToDomain(fqdnToReplace, service) {
-  const { fqdn, } = breakUrl(service);
-  const fqdnFromConfig = fqdn;
-  return service.includes(fqdnFromConfig)
-    ? service
-    : service.replace(fqdnFromConfig, fqdnToReplace);
+  const { domain, } = breakUrl(service);
+  const { domain: newDomain, } = breakUrl(fqdnToReplace);
+  return service.replace(domain, newDomain);
 }
