5e789dc0aa810a4cd13acb47e305e023a9b3edf9
EranM6 <eranm6@gmail.com>
2018-07-20 20:14:10 +0300

    feat(breadcrumbs): add google schema
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/Breadcrumbs/Breadcrumbs.js
+++ packages/components/htz-components/src/components/Breadcrumbs/Breadcrumbs.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, } from 'react';
 import PropTypes from 'prop-types';
 import { FelaComponent, FelaTheme, } from 'react-fela';
 import gql from 'graphql-tag';
@@ -25,6 +25,47 @@ const GET_BREADCRUMBS = gql`
   ${breadcrumbs}
 `;
 
+// eslint-disable-next-line react/prop-types
+const ColoredLink = ({ crumb, index, length, }) => (
+  <FelaComponent
+    key={crumb.contentId}
+    style={theme => ({
+      ...theme.type(-1),
+      fontWeight: '700',
+      marginInlineEnd: '1rem',
+      color: theme.color('neutral', '-4'),
+      ':hover': {
+        color: theme.color('neutral', '-3'),
+        textDecoration: 'underline',
+        underlineSkip: 'ink',
+      },
+      extend: [
+        theme.mq(
+          { until: 's', },
+          index !== length - 1
+            ? { display: 'none', }
+            : {}
+        ),
+        {
+          ':nth-child(odd)': {
+            color: theme.color('neutral', '-2'),
+            ':hover': {
+              color: theme.color('neutral', '-1'),
+            },
+          },
+        },
+      ],
+    })}
+    render={({ className, }) => (
+      <HtzLink
+        className={className}
+        content={crumb.name}
+        href={crumb.url}
+      />
+    )}
+  />
+);
+
 function Breadcrumbs({ articleId, className, }) {
   return (
     <Query
@@ -45,48 +86,36 @@ function Breadcrumbs({ articleId, className, }) {
           <FelaTheme
             render={theme => {
               const { breadcrumbsI18n: { ariaLabel, }, } = theme;
+              const breadcrumbsSchema = {
+                '@context': 'http://schema.org',
+                '@type': 'BreadcrumbList',
+                itemListElement: [],
+              };
               return (
-                <nav aria-label={ariaLabel} className={className}>
-                  {crumbs.map((crumb, index) => (
-                    <FelaComponent
-                      key={crumb.contentId}
-                      style={{
-                        ...theme.type(-1),
-                        fontWeight: '700',
-                        marginInlineEnd: '1rem',
-                        color: theme.color('neutral', '-4'),
-                        ':hover': {
-                          color: theme.color('neutral', '-3'),
-                          textDecoration: 'underline',
-                          underlineSkip: 'ink',
+                <Fragment>
+                  <nav aria-label={ariaLabel} className={className}>
+                    {crumbs.map((crumb, index) => {
+                      breadcrumbsSchema.itemListElement.push({
+                        '@type': 'ListItem',
+                        position: index + 1,
+                        item: {
+                          '@id': crumb.url,
+                          name: crumb.name,
                         },
-                        extend: [
-                          theme.mq(
-                            { until: 's', },
-                            index !== crumbs.length - 1
-                              ? { display: 'none', }
-                              : {}
-                          ),
-                          {
-                            ':nth-child(odd)': {
-                              color: theme.color('neutral', '-2'),
-                              ':hover': {
-                                color: theme.color('neutral', '-1'),
-                              },
-                            },
-                          },
-                        ],
-                      }}
-                      render={({ className, }) => (
-                        <HtzLink
-                          className={className}
-                          content={crumb.name}
-                          href={crumb.url}
+                      });
+                      return (
+                        <ColoredLink
+                          crumb={crumb}
+                          index={index}
+                          length={crumbs.length}
                         />
-                      )}
-                    />
-                  ))}
-                </nav>
+                      );
+                    })}
+                  </nav>
+                  <script type="application/ld+json">
+                    {JSON.stringify(breadcrumbsSchema, null, 2)}
+                  </script>
+                </Fragment>
               );
             }}
           />
