75eeffe5abdae477bc4bf8963c7ab824a21f507f
Oren Levi <oren.levi@themarker.com>
2018-07-25 17:08:14 +0300

    fix(build process): fix precommit lint not working

---
+++ scripts/lintFiles.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/**
+ * This script is used to only lint changed files
+ */
+const path = require('path');
+const spawn = require('cross-spawn');
+
+const repoDir = process.cwd();
+
+// Extract list of stages `js` files.
+const [, , ...filePaths] = process.argv;
+
+// Sort staged `js` into packages
+const packages = filePaths.reduce((result, file) => {
+  const newResult = result;
+  const pathArray = path.relative(repoDir, file).split(path.sep);
+  const packageDir = pathArray.slice(0, 3).join(path.sep);
+  const fileRelativeToPackage = pathArray.slice(3).join(path.sep);
+
+  if (newResult[packageDir]) {
+    newResult[packageDir].push(fileRelativeToPackage);
+    return newResult;
+  }
+
+  newResult[packageDir] = [fileRelativeToPackage];
+  return newResult;
+}, {});
+
+// Lint files in each package separately (in case different packages have different settings)
+Object.keys(packages).forEach(packageDir =>
+  lintLocally(packageDir, packages[packageDir])
+);
+
+function lintLocally(packageDir, files) {
+  const eslintResult = spawn.sync('yarn', ['run', 'lint', '--fix', ...files], {
+    cwd: packageDir,
+    stdio: 'inherit',
+  });
+
+  if (eslintResult.status !== 0) process.exitCode = eslintResult.status;
+}
