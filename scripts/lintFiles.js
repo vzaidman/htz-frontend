af57ae8472c2231acc67974e758875ffab5ae728
Oren Levi <oren.levi@themarker.com>
2018-07-26 16:27:02 +0300

    fix(build process): fix undeterministic lint process and add jsonlint
    Add concurrent:true to lint-staged config so that tasks will run in the order they appear rather
    than simultaniosly (pretty-quick task should complete before starting lint task)

--- scripts/lintFiles.js
+++ scripts/lintFiles.js
@@ -8,21 +8,26 @@ const spawn = require('cross-spawn');
 const repoDir = process.cwd();
 
 // Extract list of stages `js` files.
-const [, , ...filePaths] = process.argv;
+const [ , , ...filePaths ] = process.argv;
 
 // Sort staged `js` into packages
 const packages = filePaths.reduce((result, file) => {
   const newResult = result;
   const pathArray = path.relative(repoDir, file).split(path.sep);
-  const packageDir = pathArray.slice(0, 3).join(path.sep);
-  const fileRelativeToPackage = pathArray.slice(3).join(path.sep);
+  const isPackage = pathArray[0] === 'packages';
+  const packageDir = isPackage
+    ? pathArray.slice(0, 3).join(path.sep)
+    : `.${path.sep}`;
+  const fileRelativeToPackage = isPackage
+    ? pathArray.slice(3).join(path.sep)
+    : pathArray.join(path.sep);
 
   if (newResult[packageDir]) {
     newResult[packageDir].push(fileRelativeToPackage);
     return newResult;
   }
 
-  newResult[packageDir] = [fileRelativeToPackage];
+  newResult[packageDir] = [ fileRelativeToPackage, ];
   return newResult;
 }, {});
 
@@ -32,7 +37,7 @@ Object.keys(packages).forEach(packageDir =>
 );
 
 function lintLocally(packageDir, files) {
-  const eslintResult = spawn.sync('yarn', ['run', 'lint', '--fix', ...files], {
+  const eslintResult = spawn.sync('yarn', [ 'run', 'lint', '--fix', ...files, ], {
     cwd: packageDir,
     stdio: 'inherit',
   });
