3160cdc978093b3ab7fecdd4aaa4026613b140c9
Oren Levi <oren.levi@themarker.com>
2018-07-31 10:57:16 +0300

    feat(dfp parameters): add new dfp fields (#682)
    affects: @haaretz/dfp
    
    new fields:
    * htz_user_type
    * tm_user_type
    * hdc_user_type

--- packages/libs/dfp/src/objects/user.js
+++ packages/libs/dfp/src/objects/user.js
@@ -2,6 +2,58 @@
 import { UserFactory, CookieUtils, } from '@haaretz/htz-user-utils';
 import ImpressionManager from './impressionsManager';
 
+const userTypes = {
+  payer: 'payer',
+  registered: 'registered',
+  anonymous: 'anonymous',
+  trial: 'trial',
+};
+
+const productTypes = {
+  htz: 243,
+  tm: 273,
+  hdc: 239,
+  htz_tm: 274,
+};
+
+function userHasProduct(userProducts, productType, trial) {
+  return (
+    userProducts.products.filter(
+      product => product.prodNum === productType && product.trial === trial
+    ).length > 0
+  );
+}
+
+function getHdcUserType(userProducts) {
+  // user has hdc paying product
+  if (userHasProduct(userProducts, productTypes.hdc, false)) {
+    return userTypes.payer;
+  }
+  else if (userHasProduct(userProducts, productTypes.hdc, true)) {
+    // user has hdc trial product
+    return userTypes.trial;
+  }
+  return userTypes.registered;
+}
+
+function getHtzTmUserType(userProducts, productType) {
+  // user has htz/tm paying product
+  if (
+    userHasProduct(userProducts, productType, false) ||
+    userHasProduct(userProducts, productTypes.htz_tm, false)
+  ) {
+    return userTypes.payer;
+  }
+  else if (
+    userHasProduct(userProducts, productType, true) ||
+    userHasProduct(userProducts, productTypes.htz_tm, true)
+  ) {
+    // user has htz/tm trial product
+    return userTypes.trial;
+  }
+  return userTypes.registered;
+}
+
 let globalConfig;
 
 export default class DfpUser {
@@ -16,9 +68,29 @@ export default class DfpUser {
       this.ssoKey = this.ssoKey === 'tmsso' ? 'engsso' : 'tmsso';
     }
     this.type = this.user.type;
+    this.htz_type = this.getUserTypeByProduct(cookieMap, productTypes.htz);
+    this.tm_type = this.getUserTypeByProduct(cookieMap, productTypes.tm);
+    this.hdc_type = this.getUserTypeByProduct(cookieMap, productTypes.hdc);
     this.impressionManager = new ImpressionManager(config);
     this.age = this.getUserAge(cookieMap);
     this.gender = this.getUserGender(cookieMap);
+  }
+
+  getUserTypeByProduct(cookieMap, productType) {
+    let userType = userTypes.anonymous;
+    if (cookieMap && cookieMap[this.ssoKey]) {
+      if (cookieMap.userProducts) {
+        let userProducts = decodeURIComponent(cookieMap.userProducts);
+        userProducts = JSON.parse(userProducts);
+        if (productType === productTypes.hdc) {
+          userType = getHdcUserType(userProducts);
+        }
+        else {
+          userType = getHtzTmUserType(userProducts, productType);
+        }
+      }
+    }
+    return userType;
   }
 
   getUserAge(cookieMap) {
