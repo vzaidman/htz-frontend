1d1ac67f3eca8fc4475f64dab06d1180a52b4d6a
kedarguy <kedarguy@gmail.com>
2018-08-21 17:51:38 +0300

    feat(paragraph): add support for ul ol and li tags for paragraph component
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/Paragraph/Paragraph.js
+++ packages/components/htz-components/src/components/Paragraph/Paragraph.js
@@ -1,6 +1,6 @@
 import React, { Fragment, } from 'react';
 import PropTypes from 'prop-types';
-import { createComponent, FelaComponent, } from 'react-fela';
+import { FelaComponent, } from 'react-fela';
 
 import TextLink from '../TextLink/TextLink';
 import H from '../AutoLevels/H';
@@ -34,28 +34,126 @@ const P = ({ children, renderFirstImpression, ...props }) => (
   />
 );
 
-const strongStyle = () => ({
-  fontWeight: '700',
-});
-const Strong = createComponent(strongStyle, 'strong', props =>
-  Object.keys(props)
+// eslint-disable-next-line react/prop-types
+const Question = ({ children, ...props }) => (
+  <FelaComponent
+    style={theme => ({
+      fontWeight: '700',
+      extend: [
+        theme.type(1, { untilBp: 'xl', lines: 5, }),
+        theme.type(0, { fromBp: 'xl', lines: 5, }),
+      ],
+    })}
+    render={({ className, }) => (
+      <p className={className} {...props}>
+        {children}
+      </p>
+    )}
+  />
+);
+// eslint-disable-next-line react/prop-types
+const Ul = ({ children, ...props }) => (
+  <FelaComponent
+    style={theme => ({
+      marginBottom: '3rem',
+      listStylePosition: 'inside',
+      listStyleType: 'disc',
+      paddingTop: '0',
+      paddingBottom: '0',
+      paddingInlineStart: '0',
+      paddingInlineEnd: '0',
+      extend: [
+        theme.type(1, { untilBp: 'xl', lines: 5, }),
+        theme.type(0, { fromBp: 'xl', lines: 5, }),
+      ],
+    })}
+    render={({ className, }) => (
+      <ul className={className} {...props}>
+        {children}
+      </ul>
+    )}
+  />
+);
+// eslint-disable-next-line react/prop-types
+const Ol = ({ children, ...props }) => (
+  <FelaComponent
+    style={theme => ({
+      marginBottom: '3rem',
+      listStylePosition: 'inside',
+      listStyleType: 'decimal-leading-zero',
+      paddingTop: '0',
+      paddingBottom: '0',
+      paddingInlineStart: '0',
+      paddingInlineEnd: '0',
+      extend: [
+        theme.type(1, { untilBp: 'xl', lines: 5, }),
+        theme.type(0, { fromBp: 'xl', lines: 5, }),
+      ],
+    })}
+    render={({ className, }) => (
+      <ul className={className} {...props}>
+        {children}
+      </ul>
+    )}
+  />
 );
 
-const emphasisStyle = () => ({
-  fontStyle: 'italic',
-});
-const Em = createComponent(emphasisStyle, 'em');
+// eslint-disable-next-line react/prop-types
+const Strong = ({ children, ...props }) => (
+  <FelaComponent
+    style={{
+      fontWeight: '700',
+    }}
+    render={({ className, }) => (
+      <strong className={className} {...props}>
+        {children}
+      </strong>
+    )}
+  />
+);
 
-const underLineStyle = () => ({
-  textDecoration: 'underline',
-  textDecorationSkip: 'ink',
-});
-const UnderLine = createComponent(underLineStyle, 'span');
+// eslint-disable-next-line react/prop-types
+const Em = ({ children, ...props }) => (
+  <FelaComponent
+    style={{
+      fontStyle: 'italic',
+    }}
+    render={({ className, }) => (
+      <em className={className} {...props}>
+        {children}
+      </em>
+    )}
+  />
+);
 
-const spanStyle = ({ theme, }) => ({
-  backgroundColor: theme.color('primary', '-3'),
-});
-const Mark = createComponent(spanStyle, 'mark');
+// eslint-disable-next-line react/prop-types
+const UnderLine = ({ children, ...props }) => (
+  <FelaComponent
+    style={{
+      textDecoration: 'underline',
+      textDecorationSkip: 'ink',
+    }}
+    render={({ className, }) => (
+      <span className={className} {...props}>
+        {children}
+      </span>
+    )}
+  />
+);
+
+// eslint-disable-next-line react/prop-types
+const Mark = ({ children, ...props }) => (
+  <FelaComponent
+    style={theme => ({
+      backgroundColor: theme.color('primary', '-3'),
+    })}
+    render={({ className, }) => (
+      <mark className={className} {...props}>
+        {children}
+      </mark>
+    )}
+  />
+);
 
 /* Util functions */
 const extractAttributes = attrArray => {
@@ -76,7 +174,7 @@ const extractAttributes = attrArray => {
 /**
  * These type of paragraphs should ignore any MarginBottom received by the parents.
  */
-const whoShouldNotMargin = [ 'question', 'h4', ];
+const whoShouldNotMargin = [ 'question', 'h3', ];
 
 const shouldMargin = content => {
   if (whoShouldNotMargin.includes(content.tag)) {
@@ -91,19 +189,23 @@ const shouldMargin = content => {
   }
   return true;
 };
-
+// new tags: li ol ul
 const getTag = tag => {
   const tagsMap = new Map([
     [ 'p', P, ],
     [ 'a', TextLink, ],
     [ 'strong', Strong, ],
-    [ 'question', Strong, ],
+    [ 'question', Question, ],
     [ 'mark', Mark, ],
+    [ 'u', UnderLine, ],
+    [ 'ul', Ul, ],
+    [ 'ol', Ol, ],
+    [ 'li', 'li', ],
     [ 'u', UnderLine, ],
     [ 'em', Em, ],
     [ 'span', 'span', ],
     [ 'stk', 'span', ],
-    [ 'h4', H, ],
+    [ 'h3', H, ],
     [ 'br', () => <br />, ],
   ]);
   return tagsMap.get(tag);
@@ -118,6 +220,7 @@ const getTag = tag => {
 export default function Paragraph({ renderFirstImpression, ...props }) {
   /* Recursive functions */
   const Content = ({ content, }) => {
+    console.log('tagname is ul from Content ?  ', content.tag);
     const attributesObject = extractAttributes(content.attributes);
     return (
       <WrapperTag
@@ -146,6 +249,7 @@ export default function Paragraph({ renderFirstImpression, ...props }) {
     );
 
   const WrapperTag = ({ tag: tagName, content: tagElements, attributes, }) => {
+    console.log('tagname is ul from Wrapper tag ?  ', tagName);
     /**
      * Check if the Tag has a class names 'bg-brand--d'.
      * If so, it means that the Tag is actually `<mark />` (That's how the FCKEditor translates it).
