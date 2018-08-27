fdf79476106cebdea9a98f6d05cc890a8e2cb19a
sankemax <sankeman2004@gmail.com>
2018-08-27 14:57:40 +0300

    feat(login): add fela. fix document. add apolo provider
    affects: @haaretz/login

---
+++ packages/apps/login/theme/consts/campaignHeaderStyle.js
@@ -0,0 +1,25 @@
+const campaignHeaderStyle = Object.freeze({
+  // Header top
+  headerTopColorspring: '#003d59',
+  headerTopColorwinter: '#ffffff',
+  headerTopColorsummer: '#003d59',
+  headerTopColorautumn: '#ffffff',
+  // Header Main
+  headerMainColorspring: '#003d59',
+  headerMainColorwinter: '#ffffff',
+  headerMainColorsummer: '#006b96',
+  headerMainColorautumn: '#ffffff',
+  // Header bottom
+  headerBottomColorspring: '#0b7eb5',
+  headerBottomColorwinter: '#ffe70c',
+  headerBottomColorsummer: '#003d59',
+  headerBottomColorautumn: '#fff17a',
+
+  // background linear gradient
+  backgroundspring: 'linear-gradient(to right, #97ebe9, #f2fadf)',
+  backgroundwinter: 'linear-gradient(97deg, #169fd1, #105676)',
+  backgroundsummer: 'linear-gradient(91deg, #f4d835, #fff169)',
+  backgroundautumn: 'linear-gradient(to left, #08716e, #12b590)',
+});
+
+export default campaignHeaderStyle;
---
+++ packages/apps/login/theme/methods/mq.js
@@ -0,0 +1,5 @@
+import { createMqFunc, } from '@haaretz/htz-css-tools';
+import bps from '../consts/bps';
+
+const mq = createMqFunc(bps);
+export default mq;
