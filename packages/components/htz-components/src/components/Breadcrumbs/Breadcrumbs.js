c0a13172ee8143d81e151bd6a104a3d7ee471212
EranM6 <eranm6@gmail.com>
2018-08-27 11:44:21 +0300

    fix(next article): restored next article
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/Breadcrumbs/Breadcrumbs.js
+++ packages/components/htz-components/src/components/Breadcrumbs/Breadcrumbs.js
@@ -5,11 +5,12 @@ import gql from 'graphql-tag';
 import { breadcrumbs, } from '@haaretz/app-utils';
 
 import HtzLink from '../HtzLink/HtzLink';
-import { Query, } from '../ApolloBoundary/ApolloBoundary';
+import { Query, Mutation, } from '../ApolloBoundary/ApolloBoundary';
 
 const propTypes = {
-  articleId: PropTypes.string.isRequired,
   className: PropTypes.string,
+  updateArticleSection: PropTypes.func.isRequired,
+  crumbs: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
 
 const defaultProps = {
@@ -23,6 +24,12 @@ const GET_BREADCRUMBS = gql`
     }
   }
   ${breadcrumbs}
+`;
+
+const UPDATE_SECTION = gql`
+  mutation updateArticleSection($id: String!, $name: String!, $url: String!) {
+    updateArticleSection(id: $id, name: $name, url: $url) @client
+  }
 `;
 
 // eslint-disable-next-line react/prop-types
@@ -60,71 +67,103 @@ const ColoredLink = ({ crumb, index, length, }) => (
   />
 );
 
-function Breadcrumbs({ articleId, className, }) {
-  return (
-    <Query
-      query={GET_BREADCRUMBS}
-      variables={{ path: articleId, }}
-      errorPolicy="all"
-    >
-      {({ data, loading, error, }) => {
-        if (loading) return null;
-        if (error) return null;
-        if (!data || !data.page) throw new TypeError('no data !!');
-        const { lineage, } = data.page;
-        // creating a copy because when the 'steps' array is received from apollo, he is sealed.
-        const crumbs = [ ...lineage, ].reverse();
-        crumbs.shift();
-        crumbs.pop();
-        return (
-          <FelaTheme
-            render={theme => {
-              const {
-                breadcrumbsI18n: { ariaLabel, },
-              } = theme;
-              const breadcrumbsSchema = {
-                '@context': 'http://schema.org',
-                '@type': 'BreadcrumbList',
-                itemListElement: [],
-              };
-              return (
-                <Fragment>
-                  <nav aria-label={ariaLabel} className={className}>
-                    {crumbs.map((crumb, index) => {
-                      breadcrumbsSchema.itemListElement.push({
-                        '@type': 'ListItem',
-                        position: index + 1,
-                        item: {
-                          '@id': crumb.url,
-                          name: crumb.name,
-                        },
-                      });
-                      return (
-                        <ColoredLink
-                          crumb={crumb}
-                          index={index}
-                          length={crumbs.length}
-                        />
-                      );
-                    })}
-                  </nav>
-                  <script
-                    type="application/ld+json"
-                    dangerouslySetInnerHTML={{
-                      __html: JSON.stringify(breadcrumbsSchema, null, 2),
-                    }}
-                  />
-                </Fragment>
-              );
-            }}
-          />
-        );
-      }}
-    </Query>
-  );
+class Breadcrumbs extends React.Component {
+  componentDidMount() {
+    const { updateArticleSection, crumbs, } = this.props;
+    const variables = {
+      name: null,
+      id: null,
+      url: null,
+    };
+    const length = crumbs.length;
+    if (length > 0) {
+      variables.name = crumbs[length - 1].name;
+      variables.id = crumbs[length - 1].contentId;
+      variables.url = crumbs[length - 1].url;
+    }
+    updateArticleSection({
+      variables,
+    });
+  }
+
+  render() {
+    const { className, crumbs, } = this.props;
+    return (
+      <FelaTheme
+        render={theme => {
+          const {
+            breadcrumbsI18n: { ariaLabel, },
+          } = theme;
+          const breadcrumbsSchema = {
+            '@context': 'http://schema.org',
+            '@type': 'BreadcrumbList',
+            itemListElement: [],
+          };
+          return (
+            <Fragment>
+              <nav aria-label={ariaLabel} className={className}>
+                {crumbs.map((crumb, index) => {
+                  breadcrumbsSchema.itemListElement.push({
+                    '@type': 'ListItem',
+                    position: index + 1,
+                    item: {
+                      '@id': crumb.url,
+                      name: crumb.name,
+                    },
+                  });
+                  return (
+                    <ColoredLink
+                      crumb={crumb}
+                      index={index}
+                      length={crumbs.length}
+                    />
+                  );
+                })}
+              </nav>
+              <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                  __html: JSON.stringify(breadcrumbsSchema, null, 2),
+                }}
+              />
+            </Fragment>
+          );
+        }}
+      />
+    );
+  }
 }
 
 Breadcrumbs.propTypes = propTypes;
 Breadcrumbs.defaultProps = defaultProps;
 
-export default Breadcrumbs;
+// eslint-disable-next-line react/prop-types
+export default ({ articleId, ...props }) => (
+  <Query
+    query={GET_BREADCRUMBS}
+    variables={{ path: articleId, }}
+    errorPolicy="all"
+  >
+    {({ data, loading, error, }) => {
+      if (loading) return null;
+      if (error) return null;
+      if (!data || !data.page) throw new TypeError('no data !!');
+      const { lineage, } = data.page;
+      // creating a copy because when the 'steps' array is received from apollo, he is sealed.
+      const crumbs = [ ...lineage, ].reverse();
+      crumbs.shift();
+      crumbs.pop();
+      return (
+        <Mutation mutation={UPDATE_SECTION}>
+          {updateArticleSection => (
+            <Breadcrumbs
+              crumbs={crumbs}
+              {...props}
+              updateArticleSection={updateArticleSection}
+            />
+          )}
+        </Mutation>
+      );
+    }}
+  </Query>
+);
