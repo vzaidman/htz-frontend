b66492c6f089b7c46bb99343a0b1783e803815e7
sankemax <sankeman2004@gmail.com>
2018-09-12 13:55:29 +0300

    feat(login): remove unnecessary style code. add theme to index. refactor flow dispenser
    affects: @haaretz/login

--- packages/apps/login/components/FlowDispenser/flows.js
+++ packages/apps/login/components/FlowDispenser/flows.js
@@ -1,75 +1,77 @@
-export default ({
-  1: {
-    flowNumber: 1,
-    initialState: 'otpValidation',
-    initialTransition: '/otpValidation',
-    transitionRouteMap: new Map([
-      [ '-otpValidation', '/otpValidation', ],
-      [ '-otpValidation2', '/otpValidation2', ],
-      [ '-password', '/password', ],
-      [ '-phoneInput', '/phoneInput', ],
-    ]),
-    otpValidation: {
-      sendAgain: 'otpValidation2',
-      accept: 'success',
-      withPassword: 'password',
-      notMyPhone: 'phoneInput',
+/* eslint-disable comma-dangle */
+export default (
+  [
+    {
+      flowNumber: 1,
+      initialState: 'otpValidation',
+      initialTransition: '/otpValidation',
+      transitionRouteMap: new Map([
+        [ '-otpValidation', '/otpValidation' ],
+        [ '-otpValidation2', '/otpValidation2' ],
+        [ '-password', '/password' ],
+        [ '-phoneInput', '/phoneInput' ],
+      ]),
+      otpValidation: {
+        sendAgain: 'otpValidation2',
+        accept: 'success',
+        withPassword: 'password',
+        notMyPhone: 'phoneInput',
+      },
+      otpValidation2: {
+        withPassword: 'password',
+        getCustomerService: 'customerService',
+      },
+      phoneInput: {
+        withPassword: 'password',
+        accept: 'phoneMailSent',
+      },
+      phoneMailSent: {
+        sendAgain: 'phoneMailSent2',
+        withPassword: 'password',
+      },
+      phoneMailSent2: {
+        getCustomerService: 'customerService',
+        withPassword: 'password',
+      },
     },
-    otpValidation2: {
-      withPassword: 'password',
-      getCustomerService: 'customerService',
+    {
+      flowNumber: 2,
+      initialState: 'emailPhoneInput',
+      initialTransition: '/emailPhoneInput',
+      emailPhoneInput: {
+        withPassword: 'password',
+        accept: 'phoneMailSent',
+      },
+      phoneMailSent: {
+        withPassword: 'password',
+        sendAgain: 'phoneMailSent2',
+      },
+      phoneMailSent2: {
+        getCustomerService: 'customerService',
+        withPassword: 'password',
+      },
     },
-    phoneInput: {
-      withPassword: 'password',
-      accept: 'phoneMailSent',
+    {
+      flowNumber: 3,
+      initialState: 'emailValidationSent',
+      initialTransition: '/emailValidationSent',
+      emailValidationSent: {
+        sendAgain: 'emailValidationSent2',
+        notRegistered: 'register',
+      },
+      emailValidationSent2: {
+        getCustomerService: 'customerService',
+      },
     },
-    phoneMailSent: {
-      sendAgain: 'phoneMailSent2',
-      withPassword: 'password',
+    { // TODO complete flows
+      flowNumber: 4,
     },
-    phoneMailSent2: {
-      getCustomerService: 'customerService',
-      withPassword: 'password',
-    },
-  },
-  2: {
-    flowNumber: 2,
-    initialState: 'emailPhoneInput',
-    initialTransition: '/emailPhoneInput',
-    emailPhoneInput: {
-      withPassword: 'password',
-      accept: 'phoneMailSent',
-    },
-    phoneMailSent: {
-      withPassword: 'password',
-      sendAgain: 'phoneMailSent2',
-    },
-    phoneMailSent2: {
-      getCustomerService: 'customerService',
-      withPassword: 'password',
-    },
-  },
-  3: {
-    flowNumber: 3,
-    initialState: 'emailValidationSent',
-    initialTransition: '/emailValidationSent',
-    emailValidationSent: {
-      sendAgain: 'emailValidationSent2',
-      notRegistered: 'register',
-    },
-    emailValidationSent2: {
-      getCustomerService: 'customerService',
-    },
-  },
-  4: { // TODO complete flows
-    flowNumber: 4,
-  },
-  5: {
-    flowNumber: 5,
+    {
+      flowNumber: 5,
 
-  },
-  6: {
-    flowNumber: 6,
+    },
+    {
+      flowNumber: 6,
 
-  },
-});
+    },
+  ]);
