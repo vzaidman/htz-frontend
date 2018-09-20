e32633e9db65ef151697968afdb5dd2420fcbdba
Guy Aston <guy.aston@themarker.com>
2018-09-20 18:15:30 +0300

    feat(login): login-wo-password-ALL-794-V2_views_2.0
    affects: @haaretz/login, @haaretz/app-utils

--- packages/apps/login/layouts/Footer.js
+++ packages/apps/login/layouts/Footer.js
@@ -1,11 +1,29 @@
 import React, { Fragment, } from 'react';
+import { createComponent, FelaComponent, } from 'react-fela';
+import { LoginGeneralLayoutStyles, } from '../components/StyleComponents/LoginStyleComponents';
+
+const footerWrapperStyle = () => ({
+  position: 'absolute',
+  bottom: '0px',
+  width: '100%',
+  height: '315px',
+  backgroundColor: '#00537a',
+  color: '#ffffff',
+
+  '@media (max-width: 768px)': {
+    height: '150px',
+  },
+});
+const { FooterWrapper, } = LoginGeneralLayoutStyles;
 
 const Footer = () => (
   <Fragment>
-    <br />
-    <footer>
-      <div>FOOTER</div>
-    </footer>
+    <FooterWrapper>
+      <br />
+      <footer>
+        <div>FOOTER</div>
+      </footer>
+    </FooterWrapper>
   </Fragment>
 );
 
