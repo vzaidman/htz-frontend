41cea6fcbc7695ea86c1bef29158c5d49a02618a
EranM6 <eranm6@gmail.com>
2018-09-25 14:56:40 +0300

    refactor(tm-theme): fix font family default
    affects: @haaretz/tm-theme

--- packages/components/tm-theme/src/consts/fontStacks.js
+++ packages/components/tm-theme/src/consts/fontStacks.js
@@ -1,6 +1,5 @@
 const fontStacks = Object.freeze({
-  default: '"Helvetica Neue",Helvetica,Arial,sans-serif',
-  enhanced: '"Open Sans Hebrew","Helvetica Neue",Helvetica,Arial,sans-serif',
+  default: '"Open Sans Hebrew","Helvetica Neue",Helvetica,Arial,sans-serif',
   alt: '"Helvetica Neue",Helvetica,Arial,sans-serif',
 });
 
