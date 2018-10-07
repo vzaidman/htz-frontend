b0cb1fb244668a2b7c17bc93b5f15a44d8d74947
sankemax <sankeman2004@gmail.com>
2018-10-07 19:26:48 +0300

    feat(login): wIP add userInfo dev and prod uris to config
    affects: @haaretz/haaretz.co.il, @haaretz/graphql-server

--- packages/apps/haaretz.co.il/config/development.js
+++ packages/apps/haaretz.co.il/config/development.js
@@ -8,9 +8,8 @@ module.exports = {
     }),
     newSso: 'https://ms-apps.haaretz.co.il/sso-dev',
     sso: 'https://devsso.haaretz.co.il',
-    userInfoUri: 'https://ms-apps.haaretz.co.il/userInfo-dev',
-    image: 'https://images.haarets.co.il/image',
     userInfoUri: 'https://ms-apps-dev.haaretz.co.il/userInfo',
+    image: 'https://images.haarets.co.il/image',
     polopolyImageBaseHref: 'https://www.haaretz.co.il',
     graphql: defer(function () {
       return `http${
