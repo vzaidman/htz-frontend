6a8d3fc16c38dfa57a4e9d93a4d51be33ac0f209
sankemax <sankeman2004@gmail.com>
2018-10-11 21:25:49 +0300

    feat(login): wIP interface with 'send OTP'. add methods to save and retreive hash from apollo
    affects: @haaretz/login
    
    todo: fetch statement should be refactored to graphql query

--- packages/apps/login/config/production.js
+++ packages/apps/login/config/production.js
@@ -9,6 +9,11 @@ module.exports = {
     sso: 'https://sso.haaretz.co.il',
     newSso: 'https://ms-apps.haaretz.co.il/sso',
     userInfoUri: 'https://ms-apps.haaretz.co.il/userInfo',
+    otp: {
+      base: 'TODO',
+      generate: '/generate',
+      validate: '/validate',
+    },
     image: 'https://images.haarets.co.il/image',
     polopolyImageBaseHref: 'https://www.haaretz.co.il',
     graphql: defer(function () {
