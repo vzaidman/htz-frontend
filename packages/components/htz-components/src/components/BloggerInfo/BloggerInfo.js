d4521bff65f6a407fbee783367e9393f1ccc8cd8
yehudagoldner <yehuda.goldner@themarker.com>
2018-10-14 15:53:54 +0300

    feat(adding blogger info widget): adding blogger info widget
    affects: @haaretz/htz-components

---
+++ packages/components/htz-components/src/components/BloggerInfo/BloggerInfo.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FelaComponent, } from 'react-fela';
+import { borderVertical, } from '@haaretz/htz-css-tools';
+import Image from '../Image/Image';
+import Paragraph from '../Paragraph/Paragraph';
+
+const bloggerInfoWrapper = theme => ({
+  display: 'flex',
+  flexDirection: 'column',
+  extend: [
+    borderVertical({ width: 2, lines: 4, style: 'solid', color: theme.color('primary', 'base'), }),
+  ],
+});
+
+const bloggerBasicRow = theme => ({
+  display: 'flex',
+  alignItems: 'center',
+  extend: [
+    theme.mq(
+      { until: 's', },
+      {
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+      }
+    ),
+  ],
+});
+
+const bloggerImageWrapper = theme => ({
+  extend: [ theme.mq({ until: 's', }, { flex: '100%', textAlign: 'center', }), ],
+});
+
+const bloggerImage = {
+  width: [
+    { until: 'xl', value: '12rem', },
+    { from: 'xl', value: '8rem', },
+  ],
+  height: [
+    { until: 'xl', value: '12rem', },
+    { from: 'xl', value: '8rem', },
+  ],
+  borderRadius: '50%',
+  overflow: 'hidden',
+  display: [ { until: 's', value: 'inline-block', }, { from: 'l', value: 'block', }, ],
+  paddingBottom: '0',
+  marginInlineEnd: '2rem',
+};
+
+const imgOption = {
+  transforms: {
+    width: '106',
+    aspect: 'square',
+  },
+};
+
+const bloggerName = theme => ({
+  color: theme.color('primary', 'base'),
+  fontWeight: 'bold',
+  extend: [
+    theme.type(1, { untilBp: 'xl', }),
+    theme.type(0, { fromBp: 'xl', }),
+  ],
+});
+
+const bloggerNickName = theme => ({
+  color: theme.color('primary', 'base'),
+  extend: [
+    theme.type(1, { untilBp: 'xl', }),
+    theme.type(0, { fromBp: 'xl', }),
+  ],
+});
+
+const bloggerInfoText = theme => ({
+  marginTop: '2rem',
+  extend: [ theme.type(-1), theme.mq({ from: 's', }), ],
+});
+
+function BloggerInfo({ author, blogName, }) {
+  return (
+    <FelaComponent style={bloggerInfoWrapper}>
+      <FelaComponent style={bloggerBasicRow}>
+        <FelaComponent style={bloggerImageWrapper}>
+          <Image
+            attrs={{ id: 'bloggerImage', }}
+            data={author.image}
+            imgOptions={imgOption}
+            miscStyles={bloggerImage}
+          />
+        </FelaComponent>
+        <FelaComponent style={bloggerName} render="span">
+          {author.contentName}
+        </FelaComponent>
+
+        <FelaComponent render="span" style={bloggerNickName}>
+          &nbsp;| {blogName}
+        </FelaComponent>
+      </FelaComponent>
+      <FelaComponent style={bloggerInfoText}>
+        {
+          author.biography.map((p, i) => (
+            <Paragraph
+              {...p}
+              miscStyles={{
+                marginBottom: i === author.biography.length - 1
+                  ? 0
+                  : [
+                    { from: 'xl', value: 2, },
+                  ],
+                type: [
+                  { until: 'xl', value: 0, },
+                  { from: 'xl', value: -1, },
+                ],
+              }}
+            />
+          ))
+        }
+      </FelaComponent>
+    </FelaComponent>
+  );
+}
+
+BloggerInfo.propTypes = {
+  author: PropTypes.shape({
+    contentName: PropTypes.string,
+    image: PropTypes.string,
+  }).isRequired,
+  blogName: PropTypes.string.isRequired,
+};
+
+export default BloggerInfo;
---
+++ packages/components/htz-components/src/components/BloggerInfo/BloggerInfo.md
@@ -0,0 +1,75 @@
+```jsx
+<div dir="rtl" style={{ "max-width": "557px" }}>
+  <BloggerInfo
+    author={{
+      image: {
+        inputTemplate: "com.tm.Image",
+        viewMode: "FullColumnWithVerticalImage",
+        accessibility: "אמיר תיבון",
+        title: "אמיר תיבון",
+        credit: null,
+        aspects: {
+          regular: {
+            x: 0,
+            y: 3,
+            width: 403,
+            height: 302
+          },
+          full: {
+            width: 403,
+            height: 403
+          },
+          headline: {
+            x: 0,
+            y: 37,
+            width: 403,
+            height: 234
+          },
+          landscape: {
+            x: 0,
+            y: 82,
+            width: 402,
+            height: 173
+          },
+          square: {
+            x: 5,
+            y: 0,
+            width: 398,
+            height: 398
+          },
+          vertical: {
+            x: 38,
+            y: 7,
+            width: 338,
+            height: 396
+          }
+        },
+        isAnimated: false,
+        contentId: "1.3424584",
+        imgArray: [
+          {
+            imgName: "image/1911355551.jpg",
+            version: null,
+            __typename: "Img"
+          }
+        ],
+        imageType: "image",
+        contentName: "אמיר תיבון",
+        __typename: "Image"
+      },
+      __typename: "AuthorObject",
+      contentId: "1.3424581",
+      contentName: "אמיר תיבון",
+      authorType: "haaretz",
+      email: "amir.tibon@haaretz.co.il",
+      facebook: null,
+      gplus: null,
+      hasEmailAlerts: true,
+      hasPushAlerts: null,
+      inputTemplate: "com.tm.Author",
+      twitter: null,
+      url: "/misc/writers/WRITER-1.3424581"
+    }}
+  />
+</div>
+```
