7b49f9ff342346fa00e7be90cb90391f3f150a00
Oren Levi <oren.levi@themarker.com>
2018-10-17 12:16:06 +0300

    fix(left sidebar): remove horizontal seperator of last list-item
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/List/views/Zoidberg/Zoidberg.view.js
+++ packages/components/htz-components/src/components/List/views/Zoidberg/Zoidberg.view.js
@@ -4,7 +4,7 @@ import { FelaComponent, FelaTheme, } from 'react-fela';
 import { borderBottom, } from '@haaretz/htz-css-tools';
 import ListItem from '../../elements/ListItem';
 
-import GeneralAdSlot from '../../../Ads/GeneralAdSlot'
+import GeneralAdSlot from '../../../Ads/GeneralAdSlot';
 import ClickTracker from '../../../ClickTracker/ClickTrackerWrapper';
 import Image from '../../../Image/Image';
 import { PromotedItem, } from '../Leela/Leela.view';
@@ -13,141 +13,133 @@ import H from '../../../AutoLevels/H';
 import HtzLink from '../../../HtzLink/HtzLink';
 import Section from '../../../AutoLevels/Section';
 
+const listItemStyle = {
+  alignItems: 'flex-start',
+  display: 'flex',
+};
+
 // eslint-disable-next-line react/prop-types
-const Zoidberg = ({ list, lazyLoad, gaAction, biAction, listId, }) => {
-  // eslint-disable-next-line react/prop-types
-  const ClickTrackerItem = ({ item, index, }) => (
-    <ClickTracker
-      {...item}
-      render={banner => {
-        const { text, link, clicktrackerimage, } = banner;
-        return (
-          <FelaTheme
-            render={theme => (
-              <BlockLink
-                miscStyles={{
-                  alignItems: 'flex-start',
-                  display: 'flex',
-                  paddingTop: '1rem',
-                  borderBottom: [
-                    '1px',
-                    '1',
-                    'solid',
-                    theme.color('neutral', '-4'),
-                  ],
-                  borderEnd: [ '4px', 0, 'solid', theme.color('neutral', '-4'), ],
-                  ':last-child': {
-                    paddingBottom: '0',
-                    borderBottomWidth: '0',
-                    borderBottomStyle: 'none',
+const ClickTrackerItem = ({ item, index, listId, biAction, }) => (
+  <ClickTracker
+    {...item}
+    render={banner => {
+      const { text, link, clicktrackerimage, } = banner;
+      return (
+        <FelaTheme
+          render={theme => (
+            <BlockLink
+              miscStyles={{
+                ...listItemStyle,
+                marginTop: '1rem',
+                borderTop: [ '1px', 0, 'solid', theme.color('neutral', '-4'), ],
+                borderBottom: [ '1px', 0, 'solid', theme.color('neutral', '-4'), ],
+                borderEnd: [ '5px', 0, 'solid', theme.color('neutral', '-4'), ],
+              }}
+              href={link}
+              onClick={() => {
+                biAction({
+                  actionCode: 109,
+                  additionalInfo: {
+                    ArticleId: item.contentId,
+                    ListId: listId,
+                    Platform: 'desktop',
+                    NoInList: index + 1,
+                    ViewName: 'Zoidberg',
                   },
-                }}
-                href={link}
-                onClick={() => {
-                  biAction({
-                    actionCode: 109,
-                    additionalInfo: {
-                      ArticleId: item.contentId,
-                      ListId: listId,
-                      Platform: 'desktop',
-                      NoInList: index + 1,
-                      ViewName: 'Zoidberg',
+                });
+              }}
+            >
+              <PromotedItem
+                title={text}
+                image={clicktrackerimage}
+                path={link}
+                suffix
+                paragraphHeight={{ maxHeight: '9rem', }}
+                textType={-2}
+              />
+            </BlockLink>
+          )}
+        />
+      );
+    }}
+  />
+);
+
+// eslint-disable-next-line react/prop-types
+const Item = ({ title, image, path, listId, index, biAction, listLength, }) => (
+  <FelaTheme
+    render={theme => (
+      <BlockLink
+        miscStyles={{
+          ...listItemStyle,
+          marginTop: index ? '1rem' : undefined,
+          ...(index > listLength
+            ? {}
+            : { borderBottom: [ '1px', '1', 'solid', theme.color('neutral', '-4'), ], }
+          ),
+        }}
+        href={path}
+        onClick={() => {
+          biAction({
+            actionCode: 109,
+            additionalInfo: {
+              ArticleId: path.match(/(?:.*-?)(1\.\d+.*)/)[1],
+              ListId: listId,
+              Platform: 'desktop',
+              NoInList: index + 1,
+              ViewName: 'Zoidberg',
+            },
+          });
+        }}
+      >
+        <Section isFragment>
+          <FelaComponent
+            style={{
+              width: '18rem',
+              flexShrink: '0',
+              flexGrow: '0',
+            }}
+            render={({ className, theme, }) => (
+              <span className={className}>
+                <Image
+                  data={image}
+                  imgOptions={{
+                    transforms: {
+                      width: '125',
+                      aspect: 'regular',
+                      quality: 'auto',
                     },
-                  });
-                }}
-              >
-                <PromotedItem
-                  title={text}
-                  image={clicktrackerimage}
-                  path={link}
-                  suffix
-                  paragraphHeight={{ maxHeight: '9rem', }}
-                  textType={-2}
+                  }}
+                  miscStyles={{
+                    width: '18rem',
+                    flexShrink: '0',
+                    flexGrow: '0',
+                  }}
                 />
-              </BlockLink>
+              </span>
             )}
           />
-        );
-      }}
-    />
-  );
+          <FelaComponent
+            style={theme => ({
+              fontWeight: '700',
+              marginStart: '1rem',
+              flexGrow: '1',
+              extend: [ theme.type(-2), ],
+            })}
+            render={({ className, }) => (
+              <H className={className}>
+                <HtzLink href={path}>{title}</HtzLink>
+              </H>
+            )}
+          />
+        </Section>
+      </BlockLink>
+    )}
+  />
+);
 
-  // eslint-disable-next-line react/prop-types
-  const Item = ({ title, image, path, index, contentId, }) => (
-    <FelaTheme
-      render={theme => (
-        <BlockLink
-          miscStyles={{
-            alignItems: 'flex-start',
-            display: 'flex',
-            paddingTop: '1rem',
-            borderBottom: [ '1px', '1', 'solid', theme.color('neutral', '-4'), ],
-            ':last-child': {
-              paddingBottom: '0',
-              borderBottomWidth: '0',
-              borderBottomStyle: 'none',
-            },
-          }}
-          href={path}
-          onClick={() => {
-            biAction({
-              actionCode: 109,
-              additionalInfo: {
-                ArticleId: path.match(/(?:.*-?)(1\.\d+.*)/)[1],
-                ListId: listId,
-                Platform: 'desktop',
-                NoInList: index + 1,
-                ViewName: 'Zoidberg',
-              },
-            });
-          }}
-        >
-          <Section isFragment>
-            <FelaComponent
-              style={{
-                width: '18rem',
-                flexShrink: '0',
-                flexGrow: '0',
-              }}
-              render={({ className, theme, }) => (
-                <span className={className}>
-                  <Image
-                    data={image}
-                    imgOptions={{
-                      transforms: {
-                        width: '125',
-                        aspect: 'regular',
-                        quality: 'auto',
-                      },
-                    }}
-                    miscStyles={{
-                      width: '18rem',
-                      flexShrink: '0',
-                      flexGrow: '0',
-                    }}
-                  />
-                </span>
-              )}
-            />
-            <FelaComponent
-              style={theme => ({
-                fontWeight: '700',
-                marginStart: '1rem',
-                flexGrow: '1',
-                extend: [ theme.type(-2), ],
-              })}
-              render={({ className, }) => (
-                <H className={className}>
-                  <HtzLink href={path}>{title}</HtzLink>
-                </H>
-              )}
-            />
-          </Section>
-        </BlockLink>
-      )}
-    />
-  );
-
+// eslint-disable-next-line react/prop-types
+const Zoidberg = ({ list, lazyLoad, biAction, listId, }) => {
   const { title, items, } = list;
   return (
     <FelaComponent
@@ -163,7 +155,7 @@ const Zoidberg = ({ list, lazyLoad, gaAction, biAction, listId, }) => {
           fontWeight: '700',
           color: theme.color('primary'),
           ...borderBottom('2px', 1, 'solid', theme.color('primary')),
-          marginBottom: '1rem',
+          marginBottom: '2rem',
         })}
         render={({ className, }) => <H className={className}>{title}</H>}
       />
@@ -176,30 +168,20 @@ const Zoidberg = ({ list, lazyLoad, gaAction, biAction, listId, }) => {
                 <ClickTrackerItem
                   item={item}
                   index={index}
-                  gaAction={gaAction}
                   biAction={biAction}
                   listId={listId}
                 />
               ) : item.inputTemplate === 'com.polobase.DfpBannerElement' ? (
-                <FelaComponent
-                  style={theme => ({
-                    display: 'flex',
-                    paddingTop: '1rem',
-                    ':not(:last-child)': {
-                      ...borderBottom(
-                       '1px',
-                       1,
-                       'solid',
-                       theme.color('neutral', '-4')
-                    ),
-                  },
-                })}
-                >
-                  <GeneralAdSlot {...item} />
-                </FelaComponent>
+                <GeneralAdSlot
+                  {...item}
+                  styleRule={{
+                    ...listItemStyle,
+                    marginTop: '1rem',
+                  }}
+                />
               ) : null
             ) : (
-              <Item {...item} index={index} />
+              <Item {...item} index={index} biAction={biAction} listId={listId} listLength={items.length}/>
             )}
           </ListItem>
         ))}
