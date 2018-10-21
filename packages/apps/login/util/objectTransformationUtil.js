04b9b6d97f73dd54cdbd8c0b294b2cd8ba6d59f1
sankemax <sankeman2004@gmail.com>
2018-10-21 13:16:31 +0300

    feat(login): fix query and user info transformation
    affects: @haaretz/login

--- packages/apps/login/util/objectTransformationUtil.js
+++ packages/apps/login/util/objectTransformationUtil.js
@@ -1,14 +1,15 @@
 const transformGraphQlObjectToFlowDispenserObject = ({ userByMail, }) => {
-  const userStatus = userByMail.userStatus !== null;
-  const crmStatus = userByMail.userCrmStatus !== null;
+  const userExist = userByMail !== null;
+  const userStatus = userExist && userByMail.userStatus !== null;
+  const crmStatus = userExist && userByMail.userCrmStatus !== null;
   return {
     user: {
-      isUserExist: userByMail.ssoId !== null,
-      isEmailValid: userStatus ? userByMail.userStatus.isEmailValidated : false,
-      isPhoneValid: userStatus ? userByMail.userStatus.isMobileValidated : false,
-      isPhoneConnectedWithEmail: userStatus && userByMail.userStatus.isPhoneEmailConn,
+      isUserExist: userExist && userByMail.ssoId !== null,
+      isEmailValid: userExist && userStatus ? userByMail.userStatus.isEmailValidated : false,
+      isPhoneValid: userExist && userStatus ? userByMail.userStatus.isMobileValidated : false,
+      isPhoneConnectedWithEmail: userExist && userStatus && userByMail.userStatus.isPhoneEmailConn,
       isPremiumUser:
-        crmStatus &&
+        userExist && crmStatus &&
         userByMail.userCrmStatus.id !== null &&
         (userByMail.userCrmStatus.isActiveEng ||
           userByMail.userCrmStatus.isActiveHeb ||
