bd2933312db134787168dc96dce7afe524e56e9b
Guy Aston <guy.aston@themarker.com>
2018-10-23 12:38:48 +0300

    WIP(login): enable link option in tabs & footer updates
    affects: @haaretz/login

--- packages/apps/login/components/Misc/TabsFrame.js
+++ packages/apps/login/components/Misc/TabsFrame.js
@@ -1,18 +1,26 @@
 import React, { Fragment, } from 'react';
 import PropTypes from 'prop-types';
+import { HtzLink, } from '@haaretz/htz-components';
 import { LoginContentStyles, } from '../StyleComponents/LoginStyleComponents';
+
+const findRout = (route) => {
+  console.log(route);
+}
 
 export default class TabsFrame extends React.Component {
   state = {
     activeTab: 0,
+    isLink: [],
   };
 
   static propTypes = {
     activeTab: PropTypes.number,
+    isLink: PropTypes.array,
   };
 
   static defaultProps = {
     activeTab: 1,
+    isLink: [],
   };
 
   /* ---------- Lifecycle Methods ---------- */
@@ -21,14 +29,48 @@ export default class TabsFrame extends React.Component {
   }
 
   /* ------------ Functionality ------------ */
-  changeTab(index) {
+  changeTab = (index) => {
     return () => {
       this.setState({ activeTab: index, });
     };
   }
 
-  setInitTab() {
+  setInitTab = () => {
     this.setState({ activeTab: this.props.activeTab })
+  }
+
+  createNavButton = (child, index) => {
+    return !this.state.isLink[index] ?
+      (
+        <span className={index === this.state.activeTab ? "on" : ""} >
+          <input
+            type="checkbox"
+            name="tab"
+            id={`tab${index}`}
+            value={`tab${index}`}
+            onClick={this.changeTab(index)}
+          />
+          <label htmlFor={`tab${index}`} tabIndex={index}>{child.props.tabname || `Tab ${index}`}</label>
+        </span>
+      ) : 
+      (
+        <HtzLink 
+          href={`${findRout(this.state.isLink[index])}`}
+          onClick={e => {
+            e.preventDefault();
+            const route = findRout(this.state.isLink[index]);
+            //Router.push(route);
+          }}
+        >
+          {child.props.tabname || `Tab ${index}`}
+        </HtzLink>
+      );
+  }
+
+  navClickHandler = (index) => {
+    this.state.isLine[index] ?
+      this.changeTab(index) :
+      console.log("do router action");
   }
 
   /* ----------- Tabs Rendering ------------ */
@@ -37,16 +79,7 @@ export default class TabsFrame extends React.Component {
     return (
       <TopLinks>
         {React.Children.map(children, (child, i) => (
-          <span className={i === this.state.activeTab ? "on" : ""} >
-            <input
-              type="radio"
-              name="tab"
-              id={`tab${i}`}
-              value={`tab${i}`}
-              onClick={this.changeTab(i)}
-            />
-            <label htmlFor={`tab${i}`} tabIndex={i}>{child.props.tabname || `Tab ${i}`}</label>
-          </span>
+          this.createNavButton(child, i)
         ))}
       </TopLinks>
     );
