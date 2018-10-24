7dd6d6dfea30ed98e42d6c1b25a9a43973e05e83
sankemax <sankeman2004@gmail.com>
2018-10-24 17:12:41 +0300

    feat(login): wIP change export
    affects: @haaretz/login

--- packages/apps/login/util/siteUtil.js
+++ packages/apps/login/util/siteUtil.js
@@ -1,5 +1,7 @@
-export const domainToSiteNumber = domain =>
+const domainToSiteNumber = domain =>
   (domain.includes('themarker.com') ? '10'
     : domain.includes('haaretz.com') ? '85'
       : domain.includes('haaretz.co.il') ? '80'
         : '');
+
+export { domainToSiteNumber, };
