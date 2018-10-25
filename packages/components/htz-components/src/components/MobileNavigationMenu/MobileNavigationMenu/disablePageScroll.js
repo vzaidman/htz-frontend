6d1d260ca9b8f3eb9722acf847f99ba3f7686575
Oren Levi <oren.levi@themarker.com>
2018-10-25 14:40:18 +0300

    fix(mobile navbar): temporary partial fix for backdrop scroll
    affects: @haaretz/htz-components
    
    the real issue is that the mobile navbar is not part of the modal, this temprary fix is a patch that
    should be removed once this issue is resolved

--- packages/components/htz-components/src/components/MobileNavigationMenu/MobileNavigationMenu/disablePageScroll.js
+++ packages/components/htz-components/src/components/MobileNavigationMenu/MobileNavigationMenu/disablePageScroll.js
@@ -4,6 +4,8 @@ export default isOn => {
   try {
     const html = window.document.getElementsByTagName('html')[0];
     html.style.overflow = isOn ? 'hidden' : null;
+    // todo: remove the touchAction disabling once the backdrop scrolling issue is fully resolved
+    html.style.touchAction = isOn ? 'none' : null;
   }
   catch (error) { }
   return null;
