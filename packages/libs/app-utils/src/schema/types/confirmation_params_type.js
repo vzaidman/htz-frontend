325597c210427ef24b87e9f3af265180c5796223
sankemax <sankeman2004@gmail.com>
2018-10-28 14:51:46 +0200

    feat(login): fixes
    affects: @haaretz/login, @haaretz/app-utils, @haaretz/graphql-server

---
+++ packages/libs/app-utils/src/schema/types/confirmation_params_type.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { GraphQLObjectType, } from 'graphql';
+
+const ConfirmationParamsType = new GraphQLObjectType({
+  name: 'ConfirmationParamsType',
+  fields: () => ({}),
+});
+
+export default ConfirmationParamsType;
