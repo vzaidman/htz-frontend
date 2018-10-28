99790edeedcb48986952296bbac8023445bfbd36
sankemax <sankeman2004@gmail.com>
2018-10-28 18:02:30 +0200

    fix(login): fixes after rebase from master
    affects: @haaretz/login, @haaretz/app-utils, @haaretz/graphql-server

--- packages/libs/app-utils/src/schema/types/connect_mail_mobile_type.js
+++ packages/libs/app-utils/src/schema/types/connect_mail_mobile_type.js
@@ -1,5 +1,4 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-<<<<<<< HEAD
 import { GraphQLObjectType, GraphQLString } from 'graphql';
 
 const ConnectMailMobile = new GraphQLObjectType({
@@ -8,13 +7,6 @@ const ConnectMailMobile = new GraphQLObjectType({
     msg: { type: GraphQLString, },
     success: { type: GraphQLString, },
   }),
-=======
-import { GraphQLObjectType } from 'graphql';
-
-const ConnectMailMobile = new GraphQLObjectType({
-  name: 'ConnectMailMobile',
-  fields: () => ({}),
->>>>>>> feat(login): fixes
 });
 
 export default ConnectMailMobile;
