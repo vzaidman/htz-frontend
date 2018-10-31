0903d72e26bc0c7373db9370109caf270e8d1e08
sankemax <sankeman2004@gmail.com>
2018-10-31 11:51:54 +0200

    feat(haaretz.co.il): add otp application configuration (for graphql)
    affects: @haaretz/haaretz.co.il

--- packages/apps/haaretz.co.il/config/staging.js
+++ packages/apps/haaretz.co.il/config/staging.js
@@ -8,6 +8,11 @@ module.exports = {
     }),
     newSso: 'https://ms-apps-dev.haaretz.co.il/sso',
     sso: 'https://devsso.haaretz.co.il',
+    otp: {
+      base: 'https://ms-apps-dev.haaretz.co.il/otp',
+      generate: '/generate',
+      validate: '/validate',
+    },
     userInfoUri: 'https://ms-apps-dev.haaretz.co.il/userInfo',
     htzFunction: 'https://ms-apps-dev.haaretz.co.il/htz-function',
     image: 'https://images.haarets.co.il/image',
