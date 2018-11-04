ae64d4ab1173ea2b43368e86de2e5c8cbdbc4260
Oren Levi <oren.levi@themarker.com>
2018-11-04 17:53:27 +0200

    fix(comments ads): restore missing comment ads
    affects: @haaretz/htz-components
    
    fix wrong import in DfpConfProvider

--- packages/components/htz-components/src/components/Ads/DynamicAds/DfpConfProvider.js
+++ packages/components/htz-components/src/components/Ads/DynamicAds/DfpConfProvider.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Query from '../../ApolloBoundary/ApolloConsumer';
 import gql from 'graphql-tag';
+import Query from '../../ApolloBoundary/Query';
 import NoSSR from '../../NoSSR/NoSSR';
 
 const getDfpConfigQuery = gql`
