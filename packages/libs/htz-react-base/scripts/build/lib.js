a50639e37325f938f9d3872f5ed48b2cdf3713b3
yehudagoldner <yehuda.goldner@themarker.com>
2018-11-18 13:08:59 +0200

    feat(hot repload): dev workspace @haretz/haaretz.co.il watch
    affects: @haaretz/haaretz.co.il, @haaretz/htz-components, @haaretz/htz-theme, @haaretz/tm-theme,
    @haaretz/app-utils, @haaretz/d3-server, @haaretz/dfp, @haaretz/fela-utils, @haaretz/graphql-server,
    @haaretz/htz-css-tools, @haaretz/htz-react-base, @haaretz/htz-react-server, @haaretz/htz-user-utils

--- packages/libs/htz-react-base/scripts/build/lib.js
+++ packages/libs/htz-react-base/scripts/build/lib.js
@@ -8,7 +8,18 @@ checkDir('dist').then(hasDist => {
   if (!hasDist) {
     fs.mkdirSync('dist');
   }
-  if (process.argv.length < 3) {
+  if (process.argv.includes('watch')) {
+    process.argv = [
+      process.argv[0],
+      process.argv[1],
+      '--watch',
+      'src',
+      '--source-maps',
+      '--out-dir',
+      'dist/lib',
+    ];
+  }
+  else if (process.argv.length < 3) {
     process.argv.push('src', '--source-maps', '--out-dir', 'dist/lib');
   }
   require('@babel/cli/bin/babel');
