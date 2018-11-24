a49f12916e26ba72a3b28f6e6869cc112160ee48
EranM6 <eranm6@gmail.com>
2018-11-24 14:15:40 +0200

    feat(d3-server): add hotMoney endpoint

---
+++ packages/libs/d3-server/src/utils/fetchFeed.js
@@ -0,0 +1,30 @@
+import fetch from 'node-fetch';
+import * as async from 'async';
+
+export default ({ assetId, part, }) => {
+  const result = {};
+  return new Promise((resolve, reject) => {
+    async.eachOf(
+      { monthly: 2, quarterly: 7, yearly: 3, },
+      (period, key, callback) => {
+        fetch(`http://apifinance.themarker.com/TheMarkerApi/HotMoneyCharts?indexId=${assetId}&part=${part}&period=${period}`, {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json', },
+        })
+          .then(res => res.json())
+          .then(json => {
+            result[key] = json.chart.dataSource.map((asset: { time: number, value: number, }) => ({
+              time: asset[0],
+              value: asset[1],
+            }));
+            callback();
+          })
+          .catch(err => reject(err));
+      },
+      err => {
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
+};
