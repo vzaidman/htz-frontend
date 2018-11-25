383b5298809f040620516d29502a14fc6270d4d2
TxHawks <tofu.hawks@gmail.com>
2018-11-25 15:31:36 +0200

    feat(ripple): Make Ripple more configureable
    affects: @haaretz/htz-components
    
    Allow using named colors for ripple background and setting different
    values for different breakpoints
    
    Allow modifying the ripple animation duration

--- packages/components/htz-components/src/components/Animations/Ripple.js
+++ packages/components/htz-components/src/components/Animations/Ripple.js
@@ -1,43 +1,71 @@
-import { createComponent, } from 'react-fela';
+import React from 'react';
+import { FelaComponent, } from 'react-fela';
+import { parseComponentProp, } from '@haaretz/htz-css-tools';
+import setColor from '../../utils/setColor';
 
-const rippleStyle = ({ isActive, theme, bgColor, }) => ({
+const rippleStyle = ({ isActive, theme, bgColor, bgNamedColor, time, }) => ({
   position: 'absolute',
-
-  height: '8rem',
-  width: '8rem',
+  height: '3.5em',
+  width: '3.5em',
   end: '50%',
   top: '50%',
   borderRadius: '50%',
   backgroundColor: bgColor,
   display: 'block',
   opacity: 0,
-  ...(isActive
-    ? {
-      animationDuration: '0.5s',
-      animationDirection: 'alternate',
-      animationName: [
-        {
-          '0%': {
-            opacity: 0,
-            transform: 'translate(-50%, -50%) scale(0)',
-          },
+  pointerEvents: 'none',
+  animationDirection: 'alternate',
+  extend: [
+    // Set background-color
+    ...[
+      bgNamedColor
+        ? parseComponentProp(
+          'backgroundColor',
+          bgNamedColor,
+          theme.mq,
+          setColor,
+          theme.color
+        )
+        : [],
+    ],
+    // eslint-disable-next-line eqeqeq
+    theme.getDuration('animation', typeof time === 'undefined' ? 2 : time),
+    isActive
+      ? {
+        animationName: [
+          {
+            '0%': {
+              opacity: 0,
+              transform: 'translate(-50%, -50%) scale(0)',
+            },
 
-          '50%': {
-            opacity: '.3',
-          },
+            '50%': {
+              opacity: '.3',
+            },
 
-          '70%': {
-            transform: 'translate(-50%, -50%) scale(1)',
-          },
+            '70%': {
+              transform: 'translate(-50%, -50%) scale(1)',
+            },
 
-          '100%': {
-            opacity: 0,
-            transform: 'translate(-50%, -50%) scale(0)',
+            '100%': {
+              opacity: 0,
+              transform: 'translate(-50%, -50%) scale(0)',
+            },
           },
-        },
-      ],
-    }
-    : {}),
+        ],
+      }
+      : {},
+  ],
 });
 
-export default createComponent(rippleStyle);
+export default React.forwardRef(({ children, ...props }, ref) => (
+  <FelaComponent
+    {...props}
+    rule={rippleStyle}
+    render={({ className, }) => (
+      <div className={className} ref={ref}>
+        {children}
+      </div>
+    )}
+  />
+));
