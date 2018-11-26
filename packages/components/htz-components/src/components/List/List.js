8299529e7c8de9d53403387bfda99d98f0de645d
kedarguy <kedarguy@gmail.com>
2018-11-26 14:04:06 +0200

    feat(list): write and read the listDuplicationIds from the apollo store and use it when querying for
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/List/List.js
+++ packages/components/htz-components/src/components/List/List.js
@@ -1,50 +1,59 @@
-/* global localStorage */
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import gql from 'graphql-tag';
 import Query from '../ApolloBoundary/Query';
+import Mutation from '../ApolloBoundary/Mutation';
+import ApolloConsumer from '../ApolloBoundary/ApolloConsumer';
 import getView from './getView';
 import EventTracker from '../../utils/EventTracker';
 import ReadingHistoryProvider from '../ReadingHistory/ReadingHistoryProvider';
-import NoSSR from '../NoSSR/NoSSR';
 
+const UPDATE_LIST_DUPLICATION = gql`
+  mutation UpdateDuplicationList($ids: [String]) {
+    updateListDuplication(ids: $ids) @client {
+      listDuplicationIds
+    }
+  }
+`;
+const GET_LIST_DUPLICATION = gql`
+  query GetListDuplication {
+    listDuplicationIds @client
+  }
+`;
 
-const ListWithReadingHistory = props => (
-  <NoSSR>
-    <ReadingHistoryProvider>
-      {
-        readingHistory => (
-          <List history={readingHistory} {...props} />
-        )
-      }
-    </ReadingHistoryProvider>
-  </NoSSR>
+const ListWrapper = props => (
+  <Mutation mutation={UPDATE_LIST_DUPLICATION}>
+    {(updateListDuplication, data) => (
+      <ApolloConsumer>
+        {client => (
+          <List client={client} {...props} updateListDuplication={updateListDuplication} />
+        )}
+      </ApolloConsumer>
+    )}
+  </Mutation>
 );
-
-ListWithReadingHistory.propTypes = {
-  /**
-   * List's contentId.
-   */
-  contentId: PropTypes.string.isRequired,
-  /**
-   * List's view name.
-   */
-  view: PropTypes.string.isRequired,
-};
-
 
 class List extends React.Component {
   static propTypes = {
-    ...ListWithReadingHistory.propTypes,
-    history: PropTypes.arrayOf(PropTypes.string),
-  }
+    /** the apollo client instance allowing us to preform a query from the store directly */
+    client: PropTypes.shape({ readQuery: PropTypes.func, }).isRequired,
+    /**
+     * List's contentId.
+     */
+    contentId: PropTypes.string.isRequired,
+    /** A function that updates the apollo store with the itemsRepresentedContent ids  */
+    updateListDuplication: PropTypes.func.isRequired,
+    /**
+     * List's view name.
+     */
+    view: PropTypes.string.isRequired,
+  };
 
-  static defaultProps = {
-    history: [],
-  }
+  static defaultProps = {};
 
   state = {
     selectedView: null,
+    updatedListDuplication: false,
   };
 
   componentDidMount() {
@@ -58,55 +67,85 @@ class List extends React.Component {
       .catch(err => console.log(err));
   }
 
+  shouldComponentUpdate(prevProps, nextState) {
+    if (nextState.selectedView !== this.state.selectedView) return true;
+    return false;
+  }
+
   render() {
-    const { contentId, history, } = this.props;
+    const { contentId, } = this.props;
     const { selectedView, } = this.state;
     const ListComponent = selectedView
       ? Array.isArray(selectedView)
         ? selectedView[0].default
         : selectedView
       : null;
-    return ListComponent && history ? (
-      selectedView[1] ? (
-        <Query
-          query={selectedView[1].default}
-          variables={{ listId: contentId, history, }}
-        >
-          {({ data: { list, }, loading, error, }) => {
-            if (loading) return null;
-            if (error) return null;
-            const { title, items, } = {
-              title: list.title,
-              items: list.items.filter(item => Object.prototype.hasOwnProperty.call(item, 'contentId')
-              ),
-            };
 
-            return (
-              <EventTracker>
-                {({ biAction, gaAction, HtzReactGA, }) => {
-                  HtzReactGA.ga('ec:addImpression', {
-                    id: contentId,
-                    name: title,
-                    list: 'List impressions',
+    if (ListComponent) {
+      const { listDuplicationIds, } = this.props.client.readQuery({ query: GET_LIST_DUPLICATION, });
+
+      return selectedView[1] ? (
+        <ReadingHistoryProvider>
+          {readingHistory => (
+            <Query
+              query={selectedView[1].default}
+              variables={{
+                listId: contentId,
+                history: [ ...readingHistory, ...listDuplicationIds, ],
+              }}
+            >
+              {({ data: { list, }, loading, error, client, }) => {
+                if (loading) return null;
+                if (error) return null;
+                const { title, items, } = {
+                  title: list.title,
+                  // eslint-disable-next-line no-prototype-builtins
+                  items: list.items.filter(item => item.hasOwnProperty('contentId')),
+                };
+
+                const itemsRepresentedContent = items.reduce((accumulator, currentValue) => {
+                  if (currentValue && currentValue.representedContent) {
+                    accumulator.push(currentValue.representedContent);
+                  }
+                  return accumulator;
+                }, []);
+                // make sure this only runs once
+                if (!this.state.updatedListDuplication) {
+                  this.props.updateListDuplication({
+                    variables: { ids: itemsRepresentedContent, },
                   });
-                  return (
-                    <ListComponent
-                      list={{ title, items, }}
-                      listId={contentId}
-                      gaAction={gaAction}
-                      biAction={biAction}
-                    />
-                  );
-                }}
-              </EventTracker>
-            );
-          }}
-        </Query>
+                  this.setState({ updatedListDuplication: true, });
+                }
+
+                return (
+                  <EventTracker>
+                    {({ biAction, gaAction, HtzReactGA, }) => {
+                      HtzReactGA.ga('ec:addImpression', {
+                        id: contentId,
+                        name: title,
+                        list: 'List impressions',
+                      });
+                      return (
+                        <ListComponent
+                          list={{ title, items, }}
+                          listId={contentId}
+                          gaAction={gaAction}
+                          biAction={biAction}
+                        />
+                      );
+                    }}
+                  </EventTracker>
+                );
+              }}
+            </Query>
+          )}
+        </ReadingHistoryProvider>
       ) : (
         <ListComponent />
-      )
-    ) : null;
+      );
+    }
+    return null;
   }
 }
 
-export default ListWithReadingHistory;
+export default ListWrapper;
