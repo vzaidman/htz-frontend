6684dd98481e3f903c6e3cd1bff6459d4b8435ae
Oren Levi <oren.levi@themarker.com>
2018-11-26 19:00:31 +0200

    fix(finance-crash): due to import conflict in tm-theme
    affects: @haaretz/finance, @haaretz/tm-theme
    
    change component name to avoid conflict with 'fontStacks.js' file

--- packages/components/tm-theme/src/components/FontStacksExample/FontStacksExample.js
+++ packages/components/tm-theme/src/components/FontStacksExample/FontStacksExample.js
@@ -3,7 +3,7 @@ import fontStacks from '../../consts/fontStacks';
 
 const pangram = 'איך נטוס? עם גד כץ. הזקן שמחלף בצרפת!';
 
-export default function FontStacks() {
+export default function FontStacksExample() {
   return (
     <div style={{ fontSize: '1.2em' }}>
       {Object.keys(fontStacks).map(name => (
