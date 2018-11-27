930152cbef1deb5576f4ffe335e4d181759152b0
Guy Aston <guy.aston@themarker.com>
2018-11-27 13:15:46 +0200

    WIP(login): events trackers
    affects: @haaretz/login

--- packages/apps/login/pages/index.js
+++ packages/apps/login/pages/index.js
@@ -4,19 +4,14 @@ import React, { Fragment, } from 'react';
 import { ApolloConsumer, } from 'react-apollo';
 import { EventTracker, } from '@haaretz/htz-components';
 import { StyleProvider, } from '@haaretz/fela-utils';
-import { createComponent, FelaTheme, } from 'react-fela';
-import isEmail from 'validator/lib/isEmail';
+import { FelaTheme, } from 'react-fela';
 import Header from '../layouts/Header';
 import Footer from '../layouts/Footer';
 import styleRenderer from '../components/styleRenderer/styleRenderer';
 import theme from '../theme/index';
 import GET_HOST from './queries/GetHost';
 import FlowDispenser from '../components/FlowDispenser/FlowDispenser';
-import { storeFlowNumber, } from '../components/FlowDispenser/flowStorage';
 import { LoginContentStyles, } from '../components/StyleComponents/LoginStyleComponents';
-import objTransform from '../util/objectTransformationUtil';
-import { saveUserData, getDataFromUserInfo, saveOtpHash, generateOtp, mockDataFromUserInfo, } from './queryutil/userDetailsOperations';
-import { writeMetaDataToApollo, parseRouteInfo, } from '../pages/queryutil/flowUtil';
 import IndexForm from '../components/Misc/Forms/IndexForm';
 
 // Styling Components -------
