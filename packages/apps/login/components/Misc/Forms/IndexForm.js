d100fa929757233c8ce7c426283fd55b96357dd8
sankemax <sankeman2004@gmail.com>
2018-11-27 18:48:07 +0200

    feat(login): change errors to application refresh
    affects: @haaretz/login

--- packages/apps/login/components/Misc/Forms/IndexForm.js
+++ packages/apps/login/components/Misc/Forms/IndexForm.js
@@ -5,11 +5,10 @@ import isEmail from 'validator/lib/isEmail';
 import Router from 'next/router';
 import objTransform from '../../../util/objectTransformationUtil';
 import { storeFlowNumber, } from '../../FlowDispenser/flowStorage';
-import { saveUserData, getDataFromUserInfo, saveOtpHash, generateOtp, saveUserEmail, getEmail, mockDataFromUserInfo, } from '../../../pages/queryutil/userDetailsOperations';
+import { saveUserData, getDataFromUserInfo, saveOtpHash, generateOtp, saveUserEmail, validateMailWithPhone, } from '../../../pages/queryutil/userDetailsOperations';
 import { writeMetaDataToApollo, parseRouteInfo, } from '../../../pages/queryutil/flowUtil';
 import Preloader from '../../Misc/Preloader';
 import { LoginContentStyles, LoginMiscLayoutStyles, } from '../../StyleComponents/LoginStyleComponents';
-import { validateMailWithPhone, } from '../../../pages/queryutil/userDetailsOperations';
 import { sendMailValidation, } from '../../../util/requestUtil';
 import { sendTrackingEvents, } from '../../../util/trackingEventsUtil';
 
@@ -19,31 +18,29 @@ const { ErrorBox, } = LoginMiscLayoutStyles;
 // ------------------------------------
 
 // Methods ----------------------------
-const b64DecodeUnicode = (str) => {
-  return str ? decodeURIComponent(atob(str).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join('')) : null;
-}
+const b64DecodeUnicode = str => (str
+  ? decodeURIComponent(atob(str).split('').map(c => `%${(`00${c.charCodeAt(0).toString(16)}`).slice(-2)}`).join(''))
+  : null);
+
+const getParamsData = params => {
+  if (params) {
+    const decodedParams = atob(b64DecodeUnicode(params)).split('&');
+    return {
+      email: decodedParams[0].split('=')[1],
+      phone: decodedParams[1].split('=')[1],
+    };
+  }
+
+  return { email: '', phone: '', };
+};
 
 const getUrlParams = () => {
   const pageUrl = new URL(window.location.href);
   return {
     confirmation: pageUrl.searchParams.get('confirmation'),
     ...getParamsData(pageUrl.searchParams.get('params')),
-  }
-}
-
-const getParamsData = (params) => {
-  if(params) {
-    const decodedParams = atob(b64DecodeUnicode(params)).split('&');
-    return {
-      email: decodedParams[0].split('=')[1],
-      phone: decodedParams[1].split('=')[1],
-    }
-  } else {
-    return { email: '', phone: '', }
-  }
-}
+  };
+};
 
 const generateEmailError = message => [ { name: 'email', order: 1, errorText: message, }, ];
 
@@ -61,26 +58,22 @@ const vlidateEmailPhoneConnection = (client, email, autoRoute, confirmation, eve
         eventsHandler();
         return Router.push(autoRoute);
       },
-      (error) => {
-        showError(error.message)
+      error => {
+        showError(error.message);
       }
     );
-}
+};
 
-const hasValidatedPhone = (dataSaved) => {
-  return dataSaved
+const hasValidatedPhone = dataSaved => dataSaved
         && dataSaved.userData
         && dataSaved.userData.userStatus
         && dataSaved.userData.userStatus.isMobileValidated
         && dataSaved.userData.phoneNum;
-}
 
-const hasValidatedEmail = (dataSaved) => {
-  return dataSaved
+const hasValidatedEmail = dataSaved => dataSaved
         && dataSaved.userData
         && dataSaved.userData.userStatus
         && dataSaved.userData.userStatus.isEmailValidated;
-}
 
 const handleGenerateOtp = ({ phoneNum, email, ssoId, client, flow, route, showError, setPreloader, autoRoute, confirmation, eventsHandler, }) =>
   generateOtp(client)({ typeId: phoneNum, })
@@ -88,17 +81,18 @@ const handleGenerateOtp = ({ phoneNum, email, ssoId, client, flow, route, showEr
       const json = data.data.generateOtp;
       saveOtpHash(client)({ otpHash: json.hash, });
       if (json.success) {
-        if(confirmation) {
-          saveUserData(client)({ userData: { phoneNum, ssoId, __typename: "SsoUser", }, });
-          vlidateEmailPhoneConnection(client, email, autoRoute, confirmation, eventsHandler)
-        } else {
+        if (confirmation) {
+          saveUserData(client)({ userData: { phoneNum, ssoId, __typename: 'SsoUser', }, });
+          vlidateEmailPhoneConnection(client, email, autoRoute, confirmation, eventsHandler);
+        }
+        else {
           eventsHandler();
           Router.push(route);
         }
       }
       else {
         setPreloader(false);
-        showError((json.msg || "אירעה שגיאה, אנא נסה שנית מאוחר יותר."));
+        showError((json.msg || 'אירעה שגיאה, אנא נסה שנית מאוחר יותר.'));
       }
     });
 
@@ -106,26 +100,16 @@ const handleResponseFromGraphql =
   ({ client, getFlowByData, email, phone, res, showError, setPreloader, eventsTrackers, autoRoute, confirmation, }) => {
     const dataSaved = saveUserData(client)({ userData: res.userByMail, });
     const transformedObj = objTransform(res);
-
-    console.log(`data is: ${JSON.stringify(dataSaved)}, email is: ${email}`);
-
     const flow = getFlowByData(transformedObj.user);
-    if(confirmation) {
+    if (confirmation) {
       flow.flowNumber = 1;
-      flow.initialState = "loginFormsOtp";
+      flow.initialState = 'loginFormsOtp';
     }
     storeFlowNumber(client)(flow.flowNumber);
-    const eventsHandler = sendTrackingEvents(eventsTrackers ,{ page: "Main Login", flowNumber: flow.flowNumber, label: "proceedEmail", });
-
-    console.log('**** initial transition', flow.initialTransition);
-
+    const eventsHandler = sendTrackingEvents(eventsTrackers, { page: 'Main Login', flowNumber: flow.flowNumber, label: 'proceedEmail', });
     const { route, metadata, } = parseRouteInfo(flow.initialTransition);
     writeMetaDataToApollo(client, metadata);
-
-    console.log('***** route', route);
-
     if (hasValidatedPhone(dataSaved) || confirmation) {
-      console.log('mobile is validated / in confirmation phase');
       handleGenerateOtp({
         client,
         email,
@@ -140,32 +124,51 @@ const handleResponseFromGraphql =
         eventsHandler,
       });
     }
+    else if (!hasValidatedEmail(dataSaved)) {
+      sendMailValidation({ email, }).then(
+        () => {
+          eventsHandler();
+          Router.push(route);
+        },
+        error => {
+          showError((error.message || 'אירעה שגיאה'));
+        }
+      );
+    }
     else {
-      if(!hasValidatedEmail(dataSaved)) {
-        sendMailValidation({ email, }).then(
-          () => {
-            eventsHandler();
-            Router.push(route);
-          },
-          (error) => {
-            showError((error.message || "אירעה שגיאה"));
-          }
-        );
-      } else {
-        eventsHandler();
-        Router.push(route);
-      }
+      eventsHandler();
+      Router.push(route);
     }
   };
 
-const onSubmit = (client, getFlowByData, showError, hideError, setPreloader, eventsTrackers, autoRoute, confirmation) => ({ email, phone, }) => {
+const onSubmit = (
+  client,
+  getFlowByData,
+  showError,
+  hideError,
+  setPreloader,
+  eventsTrackers,
+  autoRoute,
+  confirmation
+) => ({ email, phone, }) => {
   hideError();
   setPreloader(true);
   saveUserEmail(client)(email);
   // mockDataFromUserInfo(client)(email)
   getDataFromUserInfo(client)(email)
     .then(res => {
-      handleResponseFromGraphql({ client, getFlowByData, email, phone, res, showError, setPreloader, eventsTrackers, autoRoute, confirmation });
+      handleResponseFromGraphql({
+        client,
+        getFlowByData,
+        email,
+        phone,
+        res,
+        showError,
+        setPreloader,
+        eventsTrackers,
+        autoRoute,
+        confirmation,
+      });
     })
     .catch(err => {
       setPreloader(false);
@@ -175,7 +178,6 @@ const onSubmit = (client, getFlowByData, showError, hideError, setPreloader, eve
 // ------------------------------------
 
 class IndexForm extends Component {
-
   state = {
     showError: false,
     errorMessage: '',
@@ -205,25 +207,25 @@ class IndexForm extends Component {
     this.autoSubmit(this.props);
   }
 
-  showError = (errorMsg) => {
-    this.setState({ showError: true, errorMessage: errorMsg, });
-  }
+  setPreloader = isLoadingStatus => {
+    this.setState({ isLoading: !!isLoadingStatus, });
+  };
 
   hideError = () => {
-    this.setState({ showError: false, errorMessage: "", });
-  }
+    this.setState({ showError: false, errorMessage: '', });
+  };
 
-  setPreloader = (isLoadingStatus) => {
-    this.setState({ isLoading: !!isLoadingStatus, });
-  }
+  showError = errorMsg => {
+    this.setState({ showError: true, errorMessage: errorMsg, });
+  };
 
   /**
    * the autoSubmit method runs when the user returns to the login page from a confirmation email
    */
   autoSubmit = ({ client, getFlowByData, }) => {
     const { confirmation, email, phone, } = getUrlParams();
-    const eventsTrackers = { gaAction: this.props.gaAction, biAction: this.props.biAction };
-    if(confirmation) {
+    const eventsTrackers = { gaAction: this.props.gaAction, biAction: this.props.biAction, };
+    if (confirmation) {
       const autoSubmitFunction = onSubmit(client, getFlowByData, this.showError, this.hideError, this.setPreloader, eventsTrackers, '/loginForms', confirmation);
       autoSubmitFunction({ email, phone, });
     }
@@ -233,7 +235,7 @@ class IndexForm extends Component {
   render() {
     /* :::::::::::::::::::::::::::::::::::: { RENDER :::::::::::::::::::::::::::::::::::: */
     const { client, getFlowByData, theme, gaAction, biAction, } = this.props;
-    const eventsTrackers = { gaAction: gaAction, biAction: biAction };
+    const eventsTrackers = { gaAction, biAction, };
     return (
       <FormWrapper>
         <ItemCenterer>
@@ -260,7 +262,7 @@ class IndexForm extends Component {
                 })}
               />
 
-              <ErrorBox className={this.state.showError ? "" : "hidden"}>
+              <ErrorBox className={this.state.showError ? '' : 'hidden'}>
                 <span>
                   {this.state.errorMessage}
                 </span>
@@ -274,9 +276,9 @@ class IndexForm extends Component {
           )}
         />
       </FormWrapper>
-    )
+    );
     /* :::::::::::::::::::::::::::::::::::: RENDER } :::::::::::::::::::::::::::::::::::: */
   }
-};
+}
 
 export default IndexForm;
