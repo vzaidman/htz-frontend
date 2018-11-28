5e84858ea06b7aa24859d23226b6bbb300b52c5e
Guy Aston <guy.aston@themarker.com>
2018-11-28 19:08:00 +0200

    WIP(login): add footer
    affects: @haaretz/login

--- packages/apps/login/pages/_document.js
+++ packages/apps/login/pages/_document.js
@@ -3,58 +3,13 @@ import { StyleProvider, } from '@haaretz/fela-utils';
 import loginTheme, { cssReset, fontStacks, } from '../theme';
 import styleRenderer from '../components/styleRenderer/styleRenderer';
 
-// WOFF2 files must always precede other formats in the
-// subset and variations arrays
-const fonts = {
-  'Open Sans Hebrew': {
-    applyTo: [ 'body', ],
-    subset: [
-      [
-        './static/fonts/subset/OpenSansHebrewRegularSubset.woff2',
-        './static/fonts/subset/OpenSansHebrewRegularSubset.woff',
-      ],
-      {
-        unicodeRange: 'U+05D0-U+05EA',
-      },
-    ],
-    variations: [
-      [
-        [
-          './static/fonts/OpenSansHebrewRegular.woff2',
-          './static/fonts/OpenSansHebrewRegular.woff',
-        ],
-        {
-          fontWeight: 400,
-        },
-      ],
-      [
-        [
-          './static/fonts/OpenSansHebrewLight.woff2',
-          './static/fonts/OpenSansHebrewLight.woff',
-        ],
-        {
-          fontWeight: 300,
-        },
-      ],
-      [
-        [
-          './static/fonts/OpenSansHebrewBold.woff2',
-          './static/fonts/OpenSansHebrewBold.woff',
-        ],
-        {
-          fontWeight: 700,
-        },
-      ],
-    ],
-  },
-};
 
 const loginDocument = createDocument({
   styleRenderer,
   FelaProvider: StyleProvider,
   theme: loginTheme,
-  fontRules: fonts,
-  defaultFontStack: fontStacks.default,
+  // fontRules: fonts,
+  fontStacks,
   staticRules: cssReset,
   isRtl: true,
   lang: 'heb',
