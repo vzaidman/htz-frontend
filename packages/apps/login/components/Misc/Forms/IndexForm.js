021c62ed6b868ec5f3220d4fe1388fe847e00263
Guy Aston <guy.aston@themarker.com>
2018-11-29 16:28:25 +0200

    WIP(login): added footer + updates
    affects: @haaretz/login

--- packages/apps/login/components/Misc/Forms/IndexForm.js
+++ packages/apps/login/components/Misc/Forms/IndexForm.js
@@ -329,7 +329,6 @@ class IndexForm extends Component {
           render={({ getInputProps, handleSubmit, clearForm, }) => (
             <Fragment>
               <TextInput
-                wrapperStyle={{}}
                 type="email"
                 label={theme.emailInputLabel}
                 noteText="אנא הזינו כתובת דוא”ל"
