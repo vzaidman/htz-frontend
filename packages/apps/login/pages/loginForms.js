7a75498172b17a8ab67d51cba7ae7d7d876f19fe
Guy Aston <guy.aston@themarker.com>
2018-12-03 19:20:58 +0200

    WIP(login): add tracking events to all pages
    affects: @haaretz/login

--- packages/apps/login/pages/loginForms.js
+++ packages/apps/login/pages/loginForms.js
@@ -4,7 +4,7 @@ import config from 'config';
 import Router from 'next/router';
 import { ApolloConsumer, } from 'react-apollo';
 
-import { HtzLink, Login, Form, TextInput, Button, CheckBox, } from '@haaretz/htz-components';
+import { EventTracker, HtzLink, Login, Form, TextInput, Button, CheckBox, } from '@haaretz/htz-components';
 import FSMLayout from '../layouts/FSMLayout';
 import { getUser, getEmail, getPhoneNum, getOtpHash, } from './queryutil/userDetailsOperations';
 
@@ -24,6 +24,7 @@ import { getHost, } from '../util/requestUtil';
 import { PhoneForms, } from '../components/Misc/Forms/PhoneForms';
 import PasswordForm from '../components/Misc/Forms/PasswordForm';
 import ResetPasswordForm from '../components/Misc/Forms/ResetPasswordForm';
+import { sendTrackingEvents, } from '../util/trackingEventsUtil';
 
 // Styling Components -------
 const { ContentWrapper, FormWrapper, ItemCenterer, } = LoginContentStyles;
@@ -99,85 +100,102 @@ class LoginForms extends Component {
                     <FelaTheme
                       render={theme => (
                         <Fragment>
-                          <ContentWrapper>
-                            <FormWrapper>
-                              <ItemCenterer>
-                                <h5>באפשרותכם להתחבר לאתר בשתי דרכים</h5>
-                              </ItemCenterer>
+                          <EventTracker>
+                            {({ biAction, gaAction, gaMapper, }) => (
+                              <ContentWrapper>
+                                <FormWrapper>
+                                  <ItemCenterer>
+                                    <h5>באפשרותכם להתחבר לאתר בשתי דרכים</h5>
+                                  </ItemCenterer>
 
-                              {/* ----------- Forgot Password Modal ------------ */}
-                              <LoginDialog show={this.getDialogState()} handleClose={this.hideDialog}>
-                                {
-                                  (nextStage, closeModal, CloseButton) => (
-                                    <div>
-                                      <ResetPasswordForm
-                                        nextStage={nextStage}
-                                        closeModal={closeModal}
-                                        CloseButton={CloseButton}
-                                        host={host}
-                                        theme={theme}
-                                        validateEmailInput={validateEmailInput}
-                                        client={client}
-                                      />
-                                      <div>
-                                        <CloseButton />
-                                        <h4>החלפת סיסמה</h4>
-                                        <br />
-                                        <h5>הוראות לאיפוס הסיסמה נשלחו לתיבת הדוא”ל שלך</h5>
-                                        <ItemCenterer>
-                                          <Preloader isLoading={this.state.isLoading} />
-                                          <Button onClick={closeModal}>התחברות</Button>
-                                        </ItemCenterer>
-                                      </div>
-                                    </div>
-                                  )
-                                }
-                              </LoginDialog>
+                                  {/* ----------- Forgot Password Modal ------------ */}
+                                  <LoginDialog show={this.getDialogState()} handleClose={this.hideDialog}>
+                                    {
+                                      (nextStage, closeModal, CloseButton) => (
+                                        <div>
+                                          <ResetPasswordForm
+                                            nextStage={nextStage}
+                                            closeModal={closeModal}
+                                            CloseButton={CloseButton}
+                                            host={host}
+                                            theme={theme}
+                                            validateEmailInput={validateEmailInput}
+                                            client={client}
+                                            flow={flow}
+                                            eventsTrackers={{ biAction, gaAction, }}
+                                          />
+                                          <div>
+                                            <CloseButton />
+                                            <h4>החלפת סיסמה</h4>
+                                            <br />
+                                            <h5>הוראות לאיפוס הסיסמה נשלחו לתיבת הדוא”ל שלך</h5>
+                                            <ItemCenterer>
+                                              <Preloader isLoading={this.state.isLoading} />
+                                              <Button onClick={closeModal}>התחברות</Button>
+                                            </ItemCenterer>
+                                          </div>
+                                        </div>
+                                      )
+                                    }
+                                  </LoginDialog>
 
-                              {/* ----------------- Tabs Frame ----------------- */}
-                              <TabsFrame
-                                activeTab={(flow === 5 || flow === 4) ? 1 : 0}
-                                formIndex={activeTab === 0 ? 0 : 1}
-                                findRout={findRout}
-                                doTransition={doTransition}
-                                host={host}
-                              >
-                                {/* TAB 1 */}
-                                <PhoneForms
-                                  tabname='כניסה עם קוד SMS'
-                                  flow={flow}
-                                  doTransition={doTransition}
-                                  findRout={findRout}
-                                  client={client}
-                                  user={user}
-                                />
+                                  {/* ----------------- Tabs Frame ----------------- */}
+                                  <TabsFrame
+                                    activeTab={(flow === 5 || flow === 4) ? 1 : 0}
+                                    formIndex={activeTab === 0 ? 0 : 1}
+                                    findRout={findRout}
+                                    doTransition={doTransition}
+                                    host={host}
+                                    flow={flow}
+                                    eventsTrackers={{ biAction, gaAction, }}
+                                  >
+                                    {/* TAB 1 */}
+                                    <PhoneForms
+                                      tabname='כניסה עם קוד SMS'
+                                      flow={flow}
+                                      doTransition={doTransition}
+                                      findRout={findRout}
+                                      client={client}
+                                      user={user}
+                                      flow={flow}
+                                      eventsTrackers={{ biAction, gaAction, }}
+                                      label="withSms"
+                                    />
 
-                                {/* TAB 2 */}
-                                <PasswordForm
-                                  tabname='כניסה עם סיסמה'
-                                  login={login}
-                                  theme={theme}
-                                  client={client}
-                                  showDialog={this.showDialog}
-                                  user={user}
-                                />
-                              </TabsFrame>
+                                    {/* TAB 2 */}
+                                    <PasswordForm
+                                      tabname='כניסה עם סיסמה'
+                                      login={login}
+                                      theme={theme}
+                                      client={client}
+                                      showDialog={this.showDialog}
+                                      user={user}
+                                      flow={flow}
+                                      eventsTrackers={{ biAction, gaAction, }}
+                                      label="withPassword"
+                                    />
+                                  </TabsFrame>
 
-                              <BottomLinks spacing={2.5}>
-                                <span>עדיין לא רשומים? </span>
-                                <HtzLink
-                                  href={`${findRout('registration')}`}
-                                  onClick={e => {
-                                    e.preventDefault();
-                                    const route = doTransition('registration');
-                                    Router.push(route);
-                                  }}
-                                >
-                                  הירשמו
-                                </HtzLink>
-                              </BottomLinks>
-                            </FormWrapper>
-                          </ContentWrapper>
+                                  <BottomLinks spacing={2.5}>
+                                    <span>עדיין לא רשומים? </span>
+                                    <HtzLink
+                                      href={`${findRout('registration')}`}
+                                      onClick={e => {
+                                        e.preventDefault();
+                                        const route = doTransition('registration');
+                                        sendTrackingEvents({ biAction, gaAction, }, { page: 'How to login?', flowNumber: flow, label: 'registrationPage', })(() => {
+                                            Router.push(route);
+                                          }
+                                        );
+                                      }}
+                                    >
+                                      הירשמו
+                                    </HtzLink>
+                                  </BottomLinks>
+                                </FormWrapper>
+                              </ContentWrapper>
+                            )}
+                          </EventTracker>
                         </Fragment>
                       )}
                     />
