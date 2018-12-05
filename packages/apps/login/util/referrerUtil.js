1bc1044894685d26bcbcf36ba8563e063fe4a104
Guy Aston <guy.aston@themarker.com>
2018-12-05 12:10:07 +0200

    WIP(login): referrer update
    affects: @haaretz/login

---
+++ packages/apps/login/util/referrerUtil.js
@@ -0,0 +1,20 @@
+import { getReferrer, } from '../pages/queryutil/userDetailsOperations';
+
+const regex = {
+  login: /(login-dev)|(login)|(:3000)/, // restrict referring to this url
+  site: /(https?:\/\/(.+?\.)?(themarker\.com|haaretz\.co\.il)(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)/, // allow referring to this url
+};
+
+const getCacheReferrer = client => {
+  try {
+    return getReferrer(client);
+  }
+  catch (e) {
+    return false;
+  }
+};
+
+const getReferrerUrl = (client, referrerUrl = getCacheReferrer(client)) =>
+  (!regex.login.test(referrerUrl) && regex.site.test(referrerUrl) ? referrerUrl : false);
+
+export { getReferrerUrl, };
