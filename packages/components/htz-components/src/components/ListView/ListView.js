0392f94c61110fcacea95a047018b1438258274f
TxHawks <tofu.hawks@gmail.com>
2018-12-06 16:28:13 +0200

    feat(list-view): add marginTop and miscStyles to <ListView>
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/ListView/ListView.js
+++ packages/components/htz-components/src/components/ListView/ListView.js
@@ -2,7 +2,10 @@
 
 import * as React from 'react';
 
-import type { ComponentPropResponsiveObject, } from '@haaretz/htz-css-tools';
+import type {
+  ComponentPropResponsiveObject,
+  StyleProps,
+} from '@haaretz/htz-css-tools';
 
 import Grid from '../Grid/Grid';
 import LayoutContainer from '../PageLayout/LayoutContainer';
@@ -16,21 +19,23 @@ type ListViewWrapperPropTypes = {
   children: React.Node,
   disableWrapper: boolean,
   innerBackgroundColor:
-    | string
+    | ?string
     | [string, ]
     | [string, string, ]
     | ComponentPropResponsiveObject<string | [string, ] | [string, string, ]>[],
   outerBackgroundColor:
-    | string
+    | ?string
     | [string, ]
     | [string, string, ]
     | ComponentPropResponsiveObject<string | [string, ] | [string, string, ]>[],
+  marginTop: ?number | ComponentPropResponsiveObject<number>[],
+  miscStyles: ?StyleProps,
 };
 
 type ListViewPropTypes = {
   ...ListViewWrapperPropTypes,
   gutter:
-    | number
+    | ?number
     | {
         onServerRender: number,
         queries: {
@@ -43,14 +48,48 @@ type ListViewPropTypes = {
       },
 };
 
+const defaultProps = {
+  attrs: null,
+  children: null,
+  disableWrapper: null,
+  innerBackgroundColor: null,
+  outerBackgroundColor: null,
+  marginTop: [
+    {
+      until: 's',
+      value: 4,
+    },
+    {
+      from: 's',
+      value: 8,
+    },
+  ],
+  miscStyles: null,
+};
+
+ListView.defaultProps = {
+  ...defaultProps,
+  gutter: null,
+};
+
 export default function ListView({
   attrs,
   children,
   disableWrapper,
   gutter,
+  marginTop,
   innerBackgroundColor,
   outerBackgroundColor,
+  miscStyles,
 }: ListViewPropTypes): React.Node {
+  const hasMarginTop = marginTop != null;
+  const mergedMiscStyles = miscStyles || hasMarginTop
+    ? {
+      ...(marginTop ? { marginTop, } : {}),
+      ...(miscStyles || {}),
+    }
+    : null;
+
   return (
     <ListViewWrapper
       {...{
@@ -59,18 +98,23 @@ export default function ListView({
         innerBackgroundColor,
         outerBackgroundColor,
       }}
+      miscStyles={mergedMiscStyles}
     >
       <Grid gutter={gutter}>{children}</Grid>
     </ListViewWrapper>
   );
 }
 
+ListViewWrapper.defaultProps = defaultProps;
+
 function ListViewWrapper({
   attrs,
   children,
   disableWrapper,
+  marginTop,
   innerBackgroundColor,
   outerBackgroundColor,
+  miscStyles,
 }: ListViewWrapperPropTypes): React.Node {
   return disableWrapper ? (
     <Section {...attrs}>{children}</Section>
