67d615b6e6f2778433cab81b0c1bb16848981d25
kedarguy <kedarguy@gmail.com>
2018-12-16 12:41:53 +0200

    feat(author type): deprecate credit object type and replace with one author type, change all relevan
    also small fix in ui, date was showing twice in ArticleHeaderMeta between s-l bps

--- packages/components/htz-components/src/flowTypes/AuthorDataType.js
+++ packages/components/htz-components/src/flowTypes/AuthorDataType.js
@@ -1,19 +1,17 @@
 // @flow
 import type { ImageDataType, } from './ImageDataType';
 
-export type AuthorDataType =
-  | {
-      contentName: string,
-      contentId: string,
-      inputTemplate: "com.tm.Author",
-      authorType?: string,
-      email?: string,
-      facebook?: string,
-      twitter?: string,
-      gplus?: string,
-      image?: ImageDataType,
-      hasEmailAlerts?: boolean,
-      name: string,
-      url?: string,
-    }
-  | { name: string, };
+export type AuthorDataType = {
+  contentName: string,
+  contentId: string,
+  inputTemplate: 'com.tm.Author',
+  authorType?: string,
+  email?: string,
+  facebook?: string,
+  twitter?: string,
+  gplus?: string,
+  image?: ImageDataType,
+  hasEmailAlerts?: boolean,
+  name: string,
+  url?: string,
+};
