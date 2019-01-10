c3c0d9f8093a0f155f692aa58313184c5dc8b056
sankemax <sankeman2004@gmail.com>
2019-01-10 21:34:56 +0200

    feat(login): refactor generate otp (still, need to implement the refactored form in some places in c
    affects: @haaretz/login

--- packages/apps/login/util/requestUtil.js
+++ packages/apps/login/util/requestUtil.js
@@ -1,5 +1,62 @@
 import GET_HOST from '../pages/queries/GetHost';
+import {
+  generateOtp,
+  getDataFromUserInfo,
+  getEmail,
+  getUserData,
+  saveOtpHash,
+  saveUserData,
+} from '../pages/queryutil/userDetailsOperations';
+import objTransform from './objectTransformationUtil';
 
 const getHost = client => client.readQuery({ query: GET_HOST, }).hostname.match(/^(?:.*?\.)?(.*)/i)[1];
 
-export { getHost, };
+const checkForPhoneMailConnect = userData => {
+  const transformed = objTransform({ userByMail: userData, })
+  console.log('inside check (transformed)', transformed) // TODO remove all console logs
+  return !!transformed.user.isPhoneConnectedWithEmail
+};
+
+const handleGenerateOtpIO = ({
+  client,
+  phoneNumObject,
+  miscOperations,
+  generationPredicate = () => true,
+  successCallback,
+  failCallback,
+}) => () => {
+  miscOperations();
+  const userData = getUserData(client);
+  console.log('after misc ops', userData)
+  new Promise(resolve => {
+    if (!userData.userStatus.isPhoneEmailConn) {
+      console.log('isphoneemailconn false', userData)
+      resolve(
+        getDataFromUserInfo(client)(getEmail(client))
+          .then(res => {
+            return saveUserData(client)({ userData: res.userByMail, }).userData
+          })
+      );
+    }
+    else {
+      resolve(userData);
+    }
+  })
+    .then(checkForPhoneMailConnect)
+    .then(shouldGenerate => {
+      console.log('should generate', shouldGenerate, '/ngeneration predicate', generationPredicate())
+      if (shouldGenerate && generationPredicate()) {
+        generateOtp(client)(phoneNumObject)
+          .then(data => {
+            saveOtpHash(client)({ otpHash: data.data.generateOtp.hash, });
+            return data.data.generateOtp;
+          })
+          .then(successCallback, failCallback);
+      }
+      else {
+        failCallback({ msg: 'לא ביצעתם אימות של הטלפון דרך המייל שנשלח אליכם', });
+      }
+    });
+};
+
+export { getHost, handleGenerateOtpIO, };
