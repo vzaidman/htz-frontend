3390b966fe78674523855276c5c510a3c5f59515
Guy Aston <guy.aston@themarker.com>
2019-01-16 14:14:07 +0200

    fix(login): fix IOS version checker
    affects: @haaretz/login

--- packages/apps/login/components/Misc/Forms/IndexForm.js
+++ packages/apps/login/components/Misc/Forms/IndexForm.js
@@ -38,9 +38,9 @@ const iOSversion = () => {
 };
 
 const browsersRedirect = () => {
-  const iOSVer = iOSversion()[0] || [];
+  const iOSVer = iOSversion() || [];
   const isIE = /* @cc_on!@ */false || !!document.documentMode;
-  if ((iOSVer > 0 && iOSVer < 10) || isIE) {
+  if ((iOSVer[0] > 0 && iOSVer[0] < 10) || isIE) {
     window.location.href = 'https://haaretz.co.il/misc/login-page';
   }
 };
