fc4893db282a17014af65fafac8af75edb273673
EranM6 <eranm6@gmail.com>
2019-01-16 18:21:52 +0200

    fix(slim): remove Media which broke homepage layout !!!!!!!!!
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/List/views/Slim/SlimView.js
+++ packages/components/htz-components/src/components/List/views/Slim/SlimView.js
@@ -1,6 +1,6 @@
 // @flow
-import React, { Fragment, type StatelessFunctionalComponent, } from 'react';
-import { FelaTheme, } from 'react-fela';
+import React, { Fragment, } from 'react';
+import { FelaComponent, FelaTheme, } from 'react-fela';
 
 import type { ListDataType, } from '../../../../flowTypes/ListDataType';
 import type { ListBiActionType, } from '../../../../flowTypes/ListBiActionType';
@@ -9,7 +9,6 @@ import ListView from '../../../ListView/ListView';
 import GridItem from '../../../Grid/GridItem';
 import Grid from '../../../Grid/Grid';
 import TeaserHeader from '../../../TeaserHeader/TeaserHeader';
-import Media from '../../../Media/Media';
 import { MainGallery, MobileGalleryTeaser, RelatedGallery, } from './GalleriesComponents';
 
 type Props = {
@@ -18,111 +17,112 @@ type Props = {
   gaAction: () => void,
   biAction: ListBiActionType,
 };
-const Slim: StatelessFunctionalComponent<Props> = ({
-  list,
-  listId,
-  gaAction,
-  biAction,
-}) => {
+
+function Slim({ list, listId, gaAction, biAction, }: Props) {
   const { items, } = list;
   return (
     <FelaTheme
       render={theme => (
-        <Media
-          matchOnServer
-          query={{ until: 's', }}
-        >
-          {matches => (!matches
-            ? (
-              <ListView
-                innerBackgroundColor={[ 'neutral', ]}
-                outerBackgroundColor={[ 'neutral', '-1', ]}
+        <Fragment>
+
+            <ListView
+              innerBackgroundColor={[ 'neutral', ]}
+              outerBackgroundColor={[ 'neutral', '-1', ]}
+              miscStyles={{
+                paddingStart: [ { from: 'xl', value: '4rem', }, ],
+                paddingEnd: [ { from: 'xl', value: '4rem', }, ],
+                display: [ { until: 's', value: 'none', }, ],
+              }}
+            >
+              {
+                items && items[0]
+                  ? (
+                    <Fragment>
+                      {/* Gallery Title */}
+                      <GridItem
+                        width={1}
+                        miscStyles={{
+                          paddingBottom: '1rem',
+                          paddingTop: '1rem',
+                        }}
+                      >
+                        <TeaserHeader
+                          {...items[0]}
+                          typeScale={0}
+                          color={[ 'neutral', '-10', ]}
+                          miscStyles={{
+                            paddingStart: [ { until: 'xl', value: '4rem', }, ],
+                          }}
+                        />
+                      </GridItem>
+                      {/* Main Gallery */}
+                      <GridItem
+                        width={[
+                          { until: 'xl', value: 1, },
+                          { from: 'xl', value: 9 / 12, },
+                        ]}
+                        miscStyles={{
+                          paddingBottom: [
+                            { until: 'xl', value: '2rem', },
+                          ],
+                        }}
+                      >
+                        <MainGallery item={items[0]} />
+                      </GridItem>
+                    </Fragment>
+                  )
+                  : null
+              }
+              {/* Related Galleries */}
+              <GridItem
+                width={[
+                  { until: 'xl', value: 1, },
+                  { from: 'xl', value: 3 / 12, },
+                ]}
+                stretchContent
                 miscStyles={{
-                  paddingStart: [ { from: 'xl', value: '4rem', }, ],
-                  paddingEnd: [ { from: 'xl', value: '4rem', }, ],
+                  borderTop: [
+                    { until: 'xl', value: [ '1px', 0, 'solid', theme.color('neutral', '-5'), ], },
+                  ],
                 }}
               >
-                {
-                  items && items[0]
-                    ? (
-                      <Fragment>
-                        {/* Gallery Title */}
-                        <GridItem
-                          width={1}
-                          miscStyles={{
-                            paddingBottom: '1rem',
-                            paddingTop: '1rem',
-                          }}
-                        >
-                          <TeaserHeader
-                            {...items[0]}
-                            typeScale={0}
-                            color={[ 'neutral', '-10', ]}
-                            miscStyles={{
-                              paddingStart: [ { until: 'xl', value: '4rem', }, ],
-                            }}
-                          />
-                        </GridItem>
-                        {/* Main Gallery */}
-                        <GridItem
-                          width={[
-                            { until: 'xl', value: 1, },
-                            { from: 'xl', value: 9 / 12, },
-                          ]}
-                          miscStyles={{
-                            paddingBottom: [
-                              { until: 'xl', value: '2rem', },
-                            ],
-                          }}
-                        >
-                          <MainGallery item={items[0]} />
-                        </GridItem>
-                      </Fragment>
-                    )
-                    : null
-                }
-                {/* Related Galleries */}
-                <GridItem
-                  width={[
-                    { until: 'xl', value: 1, },
-                    { from: 'xl', value: 3 / 12, },
-                  ]}
-                  stretchContent
+                <Grid
+                  gutter={2}
                   miscStyles={{
-                    borderTop: [
-                      { until: 'xl', value: [ '1px', 0, 'solid', theme.color('neutral', '-5'), ], },
-                    ],
+                    padding: [ { until: 'xl', value: '4rem', }, ],
                   }}
                 >
-                  <Grid
-                    gutter={2}
+                  <RelatedGallery item={items[1]} biAction={biAction} />
+                  <RelatedGallery item={items[2]} biAction={biAction} />
+                  <RelatedGallery
+                    item={items[3]}
+                    biAction={biAction}
                     miscStyles={{
-                      padding: [ { until: 'xl', value: '4rem', }, ],
+                      display: [
+                        { until: 'l', value: 'none', },
+                        { from: 'xl', value: 'none', },
+                      ],
                     }}
-                  >
-                    <RelatedGallery item={items[1]} biAction={biAction} />
-                    <RelatedGallery item={items[2]} biAction={biAction} />
-                    <RelatedGallery
-                      item={items[3]}
-                      biAction={biAction}
-                      miscStyles={{
-                        display: [
-                          { until: 'l', value: 'none', },
-                          { from: 'xl', value: 'none', },
-                        ],
-                      }}
-                    />
-                  </Grid>
-                </GridItem>
-              </ListView>
-            )
-            : (
-              <MobileGalleryTeaser item={items[0]} biAction={biAction} />
-            ))
-          }
-        </Media>
+                  />
+                </Grid>
+              </GridItem>
+            </ListView>
+          <FelaComponent
+            style={{
+              extend: [
+                theme.mq(
+                  { from: 's', },
+                  { display: 'none', },
+                ),
+              ],
+            }}
+          >
+            <MobileGalleryTeaser item={items[0]} biAction={biAction} />
+          </FelaComponent>
+        </Fragment>
       )}
     />
   );
-};
+}
+
 export default Slim;
