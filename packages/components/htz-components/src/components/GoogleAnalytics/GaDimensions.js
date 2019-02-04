61f39c160b227cf853e8e0ad712ee336031474e7
TawKhoury <toufe2k@gmail.com>
2019-02-04 18:08:51 +0200

    feat(add promo to pageview): send add promo to pageview, ga dimensions accept withpageview prop
    affects: @haaretz/haaretz.co.il, @haaretz/htz-components

--- packages/components/htz-components/src/components/GoogleAnalytics/GaDimensions.js
+++ packages/components/htz-components/src/components/GoogleAnalytics/GaDimensions.js
@@ -13,6 +13,8 @@ class GaDimensions extends React.Component {
     pageType: PropTypes.string,
     // User type
     userType: PropTypes.string,
+    // Set true to Track pageView manually
+    withPageView: PropTypes.bool,
   };
 
   static defaultProps = {
@@ -20,20 +22,25 @@ class GaDimensions extends React.Component {
     authors: null,
     pageType: null,
     userType: null,
+    withPageView: false,
   };
 
   componentDidMount() {
-    const { articlePaywallMode, authors, pageType, userType, } = this.props;
+    const { articlePaywallMode, authors, pageType, userType, withPageView, } = this.props;
     if (userType) {
       const visitor = userType === 'anonymous'
         ? 'Non-registered'
-        : userType === 'registered' ? 'Registered' : 'Paying';
+        : userType === 'registered'
+          ? 'Registered'
+          : 'Paying';
       ReactGA.ga('set', 'dimension1', visitor);
     }
     authors && ReactGA.ga('set', 'dimension2', authors);
     articlePaywallMode && ReactGA.ga('set', 'dimension3', articlePaywallMode);
     pageType && ReactGA.ga('set', 'dimension4', `${pageType}-REACT`);
-    ReactGA.ga('send', 'pageview');
+    if (typeof window !== 'undefined' && withPageView) {
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
   }
 
   render() {
