9b00aee99b9c1c83aac7dad5af8ef96e8ede5dc9
yehuda.goldner <goldnery@gmail.com>
2019-02-05 15:15:08 +0200

    feat: survey
    affects: @haaretz/haaretz.co.il, @haaretz/htz-components

--- packages/components/htz-components/src/components/Survey/style.js
+++ packages/components/htz-components/src/components/Survey/style.js
@@ -1,19 +1,15 @@
-const tooMenyItems = 12;
 export default {
   wrapper: { padding: '3rem', },
-  surveyWrapper: ({ theme, itemsAmount, }) => ({
+  surveyWrapper: ({ theme, mode, }) => ({
     width: '100%',
     extend: [
-      itemsAmount > tooMenyItems
+      mode === 'horizental'
         ? {}
-        : theme.mq(
-          { from: 'm', },
-          {
-            display: 'flex',
-            flexWrap: 'nowrap',
-            height: '50rem',
-          }
-        ),
+        : {
+          display: 'flex',
+          flexWrap: 'nowrap',
+          height: '50rem',
+        },
     ],
   }),
   legendsWrapper: theme => ({ textAlign: 'center', marginBottom: '2rem', }),
@@ -36,129 +32,119 @@ export default {
     width: '2rem',
     height: '2rem',
   }),
-  chartWrapper: ({ theme, itemsAmount, }) => ({
+  chartWrapper: ({ theme, mode, itemsAmount, }) => ({
     display: 'flex',
     marginBottom: '2rem',
     extend: [
-      itemsAmount > tooMenyItems
+      mode === 'horizental'
         ? {}
-        : theme.mq(
-          { from: 'm', },
-          {
-            width: `${100 / itemsAmount}%`,
-            marginBottom: '0rem',
-            flexDirection: 'column-reverse',
-            alignItems: 'center',
-          }
-        ),
+        : {
+          width: `${100 / itemsAmount}%`,
+          marginBottom: '0rem',
+          flexDirection: 'column-reverse',
+          alignItems: 'center',
+        },
     ],
   }),
-  labal: ({ theme, itemsAmount, }) => ({
+  labal: ({ theme, mode, }) => ({
     extend: [
-      itemsAmount > tooMenyItems
+      mode === 'horizental'
         ? {}
-        : theme.mq(
-          { from: 'm', },
-          {
-            position: 'absolute',
-            start: '10px',
-            whiteSpace: 'nowrap',
-          }
-        ),
+        : {
+          position: 'absolute',
+          start: '10px',
+          whiteSpace: 'nowrap',
+        },
     ],
   }),
-  labalWrapper: ({ theme, itemsAmount, }) => ({
+  labalWrapper: ({ theme, mode, }) => ({
     display: 'flex',
     ...theme.type(-2),
     extend: [
-      itemsAmount > tooMenyItems
-        ? {}
-        : theme.mq(
-          { from: 'm', },
-          {
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            width: '0rem',
-            position: 'relative',
-            textAlign: 'start',
-            transform: 'rotate(-72deg)',
-            ...theme.type(-2),
-          }
-        ),
-      theme.mq(itemsAmount > tooMenyItems ? { until: 'xl', } : { until: 'm', }, {
-        textAlign: 'end',
-        flexBasis: '28%',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        marginInlineEnd: '2%',
-      }),
+      mode === 'horizental'
+        ? {
+          textAlign: 'end',
+          flexBasis: '28%',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          marginInlineEnd: '2%',
+        }
+        : {
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+          width: '0rem',
+          position: 'relative',
+          textAlign: 'start',
+          transform: 'rotate(-72deg)',
+          ...theme.type(-2),
+        },
     ],
   }),
-  barsWrapper: ({ theme, itemsAmount, }) => ({
+  barsWrapper: ({ theme, mode, }) => ({
     display: 'flex',
     flex: '70%',
     flexDirection: 'column',
     height: '5rem',
     extend: [
-      itemsAmount > tooMenyItems
+      mode === 'horizental'
         ? {}
-        : theme.mq(
-          { from: 'm', },
-          {
-            flexDirection: 'row',
-            height: 'auto',
-          }
-        ),
+        : {
+          flexDirection: 'row',
+          height: 'auto',
+        },
     ],
   }),
-  barWrapper: ({ theme, itemsAmount, }) => ({
+  barWrapper: ({ theme, mode, }) => ({
     display: 'flex',
     flex: 'calc(70% - 4rem)',
     extend: [
-      itemsAmount > tooMenyItems
+      mode === 'horizental'
         ? {}
-        : theme.mq(
-          { from: 'm', },
-          {
-            flexDirection: 'column-reverse',
-            justifyContent: 'flex-start',
-          }
-        ),
+        : {
+          flexDirection: 'column-reverse',
+          justifyContent: 'flex-start',
+        },
     ],
   }),
-  barValue: ({ theme, itemsAmount, }) => ({
+  barValue: ({ theme, mode, }) => ({
     textAlign: 'center',
     ...theme.type(-2),
     flexBasis: 'auto',
     marginInlineStart: '0.5rem',
     extend: [
-      itemsAmount > tooMenyItems
+      mode === 'horizental'
         ? {}
-        : theme.mq(
-          { from: 'm', },
-          {
-            marginInlineStart: '0',
-          }
-        ),
+        : {
+          marginInlineStart: '0',
+        },
     ],
   }),
-  bar: ({ theme, value, barColor, itemsAmount, }) => ({
+  bar: ({ theme, value, barColor, mode, }) => ({
     background: theme.color(...barColor),
-    width: `${(Math.max(value, 1) / 40) * 100}% `,
+    width: mode === 'hide' ? '0' : `${(Math.max(value, 1) / 40) * 100}% `,
     marginTop: '0rem',
+    ...theme.getDelay('transition', 1),
+    ...theme.getDuration('transition', 1),
+    ...theme.getTimingFunction('transition', 'linear'),
     extend: [
-      itemsAmount > tooMenyItems
-        ? {}
-        : theme.mq(
-          { from: 'm', },
-          {
-            width: '2rem',
-            height: `${(Math.max(value, 1) / 40) * 100}% `,
-          }
-        ),
+      mode === 'horizental'
+        ? {
+          transitionProperty: 'width',
+        }
+        : {
+          width: '2rem',
+          transitionProperty: 'height',
+          height: mode === 'hide' ? '0' : `${(Math.max(value, 1) / 40) * 100}% `,
+        },
     ],
   }),
   comment: theme => ({
     ...theme.type(-1),
+    marginTop: '3rem',
   }),
+  buttonWrapper: {
+    textAlign: 'center',
+    marginTop: '15rem',
+  },
+  button: theme => ({}),
 };
