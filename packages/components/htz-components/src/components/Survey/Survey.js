09186fda18978478a9dd3a05627a86d16169a2c4
yehuda.goldner <goldnery@gmail.com>
2019-02-07 14:11:37 +0200

    feat: surveys phase 3 animation support
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/Survey/Survey.js
+++ packages/components/htz-components/src/components/Survey/Survey.js
@@ -1,7 +1,9 @@
 /* global window */
 import React from 'react';
 import { FelaComponent, } from 'react-fela';
+import _ from 'lodash';
 import gql from 'graphql-tag';
+import Observer from 'react-intersection-observer';
 import Query from '../ApolloBoundary/Query';
 
 import style from './style';
@@ -32,10 +34,19 @@ export default class Survey extends React.Component {
     let unsortedItems;
     this.colors = [ [ 'primary', ], [ 'quaternary', '+2', ], [ 'primary', '-3', ], [ 'black', ], ];
 
-    const { comment, mobileMode, tabletMode, desktopMode, legend, ...data } = this.xls.surveys
-      ? this.xls.surveys[0]
-      : this.xls.survey;
+    const {
+      comment,
+      showAnimationOnMobile,
+      showAnimationOnDesktop,
+      showAnimationOntablet,
+      mobileMode,
+      tabletMode,
+      desktopMode,
+      legend,
+      ...data
+    } = this.xls.surveys ? this.xls.surveys[0] : this.xls.survey;
     this.comment = comment;
+
     if (this.xls.surveys) {
       unsortedItems = Object.keys(data);
       // eslint-disable-next-line array-callback-return
@@ -47,31 +58,36 @@ export default class Survey extends React.Component {
       this.xls.surveys.map(item => {
         unsortedItems.map(v => this.partiesValues[v].push(parseInt(item[v], 10)));
       });
-      unsortedItems.sort();
-      const items = unsortedItems.sort(
-        (a, b) => this.partiesValues[b][0]
-          + this.partiesValues[b][1]
-          - (this.partiesValues[a][0] + this.partiesValues[a][1])
-      );
 
-      this.state = {
-        items,
-      };
+      this.items = _.orderBy(
+        unsortedItems,
+        v => this.partiesValues[v][1] + this.partiesValues[v][0],
+        [ 'desc', ]
+      );
     }
   }
 
   getDeviceMode(deviceType) {
-    const { mobileMode, tabletMode, desktopMode, } = this.xls.surveys[0];
+    const {
+      mobileMode,
+      tabletMode,
+      desktopMode,
+      showAnimationOnMobile,
+      showAnimationOnDesktop,
+      showAnimationOnTablet,
+    } = this.xls.surveys[0];
     switch (deviceType) {
       case 'mobile':
+        this.showAnimation = showAnimationOnMobile;
         return mobileMode || 'horizental';
       case 'tablet':
+        this.showAnimation = showAnimationOnTablet;
         return tabletMode || 'horizental';
-
       case 'desktop':
+        this.showAnimation = showAnimationOnDesktop;
         return desktopMode || 'horizental';
-
       default:
+        this.showAnimation = false;
         return 'horizental';
     }
   }
@@ -85,70 +101,81 @@ export default class Survey extends React.Component {
           let mode = 'horizental';
           mode = this.getDeviceMode(data.userAgent.deviceType);
           return (
-            <FelaComponent mode={mode} rule={style.wrapper}>
-              <FelaComponent style={style.legendsWrapper}>
-                {this.xls.surveys.map((v, i) => (
-                  <FelaComponent style={style.legend} key={this.colors[i]}>
-                    <FelaComponent color={this.colors[i]} rule={style.legendcolor} render="span" />
-                    <FelaComponent style={style.legendLabel} render="span">
-                      {v.legend}
-                    </FelaComponent>
-                  </FelaComponent>
-                ))}
-              </FelaComponent>
-              <FelaComponent mode={mode} rule={style.surveyWrapper}>
-                {this.state.items.map(item => (
-                  <FelaComponent
-                    itemsAmount={this.state.items.length}
-                    mode={mode}
-                    key={item}
-                    rule={style.chartWrapper}
-                  >
-                    <FelaComponent mode={mode} rule={style.labalWrapper} render="label">
-                      <FelaComponent mode={mode} rule={style.labal} render="label">
-                        {item}
-                      </FelaComponent>
-                    </FelaComponent>
-                    <FelaComponent mode={mode} rule={style.barsWrapper}>
-                      {this.partiesValues[item].map((v, i) => (
-                        <FelaComponent
-                          mode={mode}
-                          key={item + this.colors[i]}
-                          rule={style.barWrapper}
-                        >
+            <Observer rootMargin="50px">
+              {inView => {
+                const load = this.showAnimation ? inView : true;
+                return (
+                  <FelaComponent mode={mode} rule={style.wrapper}>
+                    <FelaComponent style={style.legendsWrapper}>
+                      {this.xls.surveys.map((v, i) => (
+                        <FelaComponent style={style.legend} key={this.colors[i]}>
                           <FelaComponent
-                            mode={mode}
-                            barColor={this.colors[i]}
-                            value={v}
-                            load
-                            rule={style.bar}
+                            color={this.colors[i]}
+                            rule={style.legendcolor}
+                            render="span"
                           />
-                          <FelaComponent mode={mode} rule={style.barValue}>
-                            {v}
+                          <FelaComponent style={style.legendLabel} render="span">
+                            {v.legend}
                           </FelaComponent>
                         </FelaComponent>
                       ))}
                     </FelaComponent>
-                  </FelaComponent>
-                ))}
-              </FelaComponent>
-              <FelaComponent mode={mode} rule={style.buttonWrapper}>
-                <Button
-                  href="https://www.haaretz.co.il/EXT-INTERACTIVE-1.6826451"
-                  style={style.button}
-                >
-                  לכל הסקרים
-                </Button>
-              </FelaComponent>
+                    <FelaComponent mode={mode} rule={style.surveyWrapper}>
+                      {this.items.map(item => (
+                        <FelaComponent
+                          itemsAmount={this.items.length}
+                          mode={mode}
+                          key={item}
+                          rule={style.chartWrapper}
+                        >
+                          <FelaComponent mode={mode} rule={style.labalWrapper} render="label">
+                            <FelaComponent mode={mode} rule={style.labal} render="label">
+                              {item}
+                            </FelaComponent>
+                          </FelaComponent>
+                          <FelaComponent mode={mode} rule={style.barsWrapper}>
+                            {this.partiesValues[item].map((v, i) => (
+                              <FelaComponent
+                                mode={mode}
+                                key={item + this.colors[i]}
+                                rule={style.barWrapper}
+                              >
+                                <FelaComponent
+                                  mode={mode}
+                                  barColor={this.colors[i]}
+                                  value={v}
+                                  load={load}
+                                  rule={style.bar}
+                                />
+                                <FelaComponent mode={mode} rule={style.barValue}>
+                                  {v}
+                                </FelaComponent>
+                              </FelaComponent>
+                            ))}
+                          </FelaComponent>
+                        </FelaComponent>
+                      ))}
+                    </FelaComponent>
+                    <FelaComponent mode={mode} rule={style.buttonWrapper}>
+                      <Button
+                        href="https://www.haaretz.co.il/EXT-INTERACTIVE-1.6826451"
+                        style={style.button}
+                      >
+                        לכל הסקרים
+                      </Button>
+                    </FelaComponent>
 
-              <FelaComponent style={style.comment}>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: this.comment.replace('*', '<br>*'),
-                  }}
-                />
-              </FelaComponent>
-            </FelaComponent>
+                    <FelaComponent style={style.comment}>
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: this.comment.replace('*', '<br>*'),
+                        }}
+                      />
+                    </FelaComponent>
+                  </FelaComponent>
+                );
+              }}
+            </Observer>
           );
         }}
       </Query>
