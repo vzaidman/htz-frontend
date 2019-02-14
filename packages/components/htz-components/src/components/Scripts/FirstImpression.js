09b1cbdf12e25b5f1c2a0ab55f58c2af5a8ccdc5
tomer.banin <banin.tomer@gmail.com>
2019-02-14 16:06:05 +0200

    feat(scripts): remove first impression
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/Scripts/FirstImpression.js
+++ packages/components/htz-components/src/components/Scripts/FirstImpression.js
@@ -12,16 +12,7 @@ class FirstImpression extends Component {
   }
 
   getInnerHtml = () => `
-    window.apd_options = { 'websiteId': 5339, 'runFromFrame': false };
-        (function() {
-            var w = window.apd_options.runFromFrame ? window.top : window;
-            if(window.apd_options.runFromFrame && w!=window.parent) w=window.parent;
-            if (w.location.hash.indexOf('apdAdmin') != -1){if(typeof(Storage) !== 'undefined') {w.localStorage.apdAdmin = 1;}}
-            var adminMode = ((typeof(Storage) == 'undefined') || (w.localStorage.apdAdmin == 1));
-            w.apd_options=window.apd_options;
-            var apd = w.document.createElement('script'); apd.type = 'text/javascript'; apd.async = true;
-            apd.src = '//' + (adminMode ? 'cdn' : 'ecdn') + '.firstimpression.io/' + (adminMode ? 'fi.js?id=' + window.apd_options.websiteId : 'fi_client.js') ;
-            var s = w.document.getElementsByTagName('head')[0]; s.appendChild(apd);
+  console.log('FIRST IMPRESSION MY ASS');
   })();
 
     `;
