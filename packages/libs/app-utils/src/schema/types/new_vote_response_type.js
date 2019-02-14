3bff84660ea7896f3b9851754d3de0e7dd87c752
TawKhoury <toufe2k@gmail.com>
2019-02-14 14:00:59 +0200

    fix(new_vote_response): fix status type to get valid response in networks
    affects: @haaretz/app-utils
    
    comments

--- packages/libs/app-utils/src/schema/types/new_vote_response_type.js
+++ packages/libs/app-utils/src/schema/types/new_vote_response_type.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { GraphQLObjectType, GraphQLInt, } from 'graphql';
+import { GraphQLObjectType, GraphQLString, } from 'graphql';
 
 const NewVoteResponseType = new GraphQLObjectType({
   name: 'NewVoteResponse',
   fields: () => ({
-    status: { type: GraphQLInt, },
+    status: { type: GraphQLString, },
   }),
 });
 
